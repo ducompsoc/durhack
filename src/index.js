64e6dba717fff43344556e482456e07f55054f05
ksouthern <karl.southern@durham.ac.uk>
2020-02-25 15:24:02 +0000

    Finished updating NextGen

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@
  */
 import tippy from 'tippy.js';
 
-import config from '../config.json';
-
 import './styles/index.scss';
 import 'tippy.js/dist/tippy.css';
 
+let config;
+if (window.location.pathname.includes('nextgen')) {
+	config = require('../config-nextgen.json');
+} else {
+	config = require('../config.json');
+}
+	
 /**
  * Find all the navigation buttons and measure them.
  */
