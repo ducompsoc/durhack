69a176460f912dfff94b2c7ea7d5997a0a750495
Ethan Waite <ethan@streamjar.tv>
2022-02-07 15:43:20 +0000

    feat: add some sponsors (#57)

--- src/index.js
+++ src/index.js
@@ -112,7 +112,7 @@ teamColumns.push(config.team.filter((_, index) => index >= limit * 3));
  * Include partials.
  */
 document.querySelector('.faqs .template').innerHTML = require('./templates/faqs.hbs')({ questions: config.faqs });
-//document.querySelector('.sponsors .template').innerHTML = require('./templates/sponsors.hbs')({ sponsors: config.sponsors });
+document.querySelector('.sponsors .template').innerHTML = require('./templates/sponsors.hbs')({ sponsors: config.sponsors });
 //document.querySelector('.schedule .template').innerHTML = require('./templates/schedule.hbs')({ schedule: config.schedule });
 
 if (document.querySelector('.team')) {
