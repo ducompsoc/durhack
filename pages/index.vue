8dc778dcd393bafdddebb897da16f3a7669ccfd1
Aaron Cheung <21007144+AaronCheung430@users.noreply.github.com>
2022-09-24 23:20:56 +0800

    :bento: rearranged sponsors logo

--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,7 @@
                             <div>
 
                                 <div class="logo">
-                                    <img
-                                        alt="DurHack 2022 Logo: A purple glowing cyberpunk triangle with a shield in the centre."
+                                    <img alt="DurHack 2022 Logo: A purple glowing cyberpunk triangle with a shield in the centre."
                                         src="/assets/logo22.png">
                                 </div>
 
@@ -56,7 +55,7 @@
                 <div class="video-front"></div>
 
                 <video autoplay muted playsinline loop poster="/assets/videos/splash-poster.jpg">
-                    <source src="/assets/videos/splash.webm" type="video/webm"/>
+                    <source src="/assets/videos/splash.webm" type="video/webm" />
                 </video>
 
                 <!--<div class="nodes">
@@ -68,11 +67,11 @@
         </div>
 
         <a id="mlh-trust-badge"
-           style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
-           href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=black"
-           target="_blank"><img
-            src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-black.svg"
-            alt="Major League Hacking 2023 Hackathon Season" style="width:100%"></a>
+            style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
+            href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=black"
+            target="_blank"><img
+                src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-black.svg"
+                alt="Major League Hacking 2023 Hackathon Season" style="width:100%"></a>
 
         <div class="midnav">
             <ul>
@@ -90,15 +89,13 @@
 
             <div class="flex-container">
                 <div class="flex-item">
-                    <img
-                        alt="A top down view of 5 students around a table with their laptops"
-                        class="file-crop"
+                    <img alt="A top down view of 5 students around a table with their laptops" class="file-crop"
                         src="assets/photos/top_down_laptop_table.jpg">
                     <div class="img-file-under-tic"></div>
 
                     <p>
-                        The premise: work together in your team to create something awesome <span
-                        class="emphasis">in just 24 hours</span>.
+                        The premise: work together in your team to create something awesome <span class="emphasis">in
+                            just 24 hours</span>.
                     </p>
 
                     <ul>
@@ -118,15 +115,12 @@
                 </div>
 
                 <div class="flex-item">
-                    <img
-                        alt="Four people in front of a whiteboard, writing and designing a progream."
-                        class="file-crop"
+                    <img alt="Four people in front of a whiteboard, writing and designing a progream." class="file-crop"
                         src="assets/photos/whiteboard.jpeg">
                     <div class="img-file-under-tic"></div>
 
                     <p>
-                        DurHack welcomes students of <span
-                        class="emphasis">all experience levels</span>,
+                        DurHack welcomes students of <span class="emphasis">all experience levels</span>,
                         from all degrees and any university.
                     </p>
 
@@ -153,32 +147,31 @@
             <div class="faq-cap top"></div>
             <aside class="inside">
                 <h1>FAQs</h1>
-                <FAQs/>
+                <FAQs />
             </aside>
             <div class="faq-cap bottom"></div>
         </div>
 
         <div class="section anchor sponsors" id="sponsors">
-            <!-- <div class="container">
-                <aside class="inside">
-                    <h1>Sponsors</h1>
-                </aside>
-
-				<div class="template"></div>
-
-				<p>Thanks in advance to all our sponsors for making DurHack possible!</p>
-
-				<p>Interested in sponsoring DurHack? We'd love to work with you. Send us an email at <a href="mailto:sponsor@durhack.com">sponsor@durhack.com</a>.</p>
-            </div> -->
             <div class="SponsorText">
                 <!-- <aside class="inside"> -->
-                    <h1>Sponsors</h1>
+                <h1>Sponsors</h1>
                 <!-- </aside> -->
             </div>
             <div class="Sponsors">
                 <Sponsors />
+
+                <!-- <div class="template"></div> -->
+
+                <p>Thanks in advance to all our sponsors for making DurHack possible!</p>
+
+                <p>Interested in sponsoring DurHack? We'd love to work with you. Send us an email at <a
+                        href="mailto:sponsor@durhack.com">sponsor@durhack.com</a>.</p>
             </div>
+
         </div>
+
+
 
 
         <div class="section anchor schedule" id="schedule">
@@ -200,10 +193,11 @@
                     <a href="/tickets" class="register button">Pre-Register now</a>
                 </div>
                 <div class="flex-item">
-                    <div  id="hoop1">
+                    <div id="hoop1">
                         <div id="hoop2">
                             <a href="https://goo.gl/maps/rmFLyMWWXC5c8im69" target="_blank">
-                                <img src="assets/mcsmap.png" alt="Map showing the location of the MCS building in Durham.">
+                                <img src="assets/mcsmap.png"
+                                    alt="Map showing the location of the MCS building in Durham.">
                             </a>
                         </div>
                     </div>
@@ -225,12 +219,14 @@
 
 
         <div class="section anchor team" id="team">
-			<div class="bg"></div>
-			<div class="inner">
-				<div class="team-grid row center template"><Team /></div>
+            <div class="bg"></div>
+            <div class="inner">
+                <div class="team-grid row center template">
+                    <Team />
+                </div>
                 <!-- <Team /> -->
-				<p><a href="mailto:hello@durhack.com">hello@durhack.com</a></p>
-			</div>
+                <p><a href="mailto:hello@durhack.com">hello@durhack.com</a></p>
+            </div>
         </div>
 
         <!-- <div class="section anchor team" id="team">
@@ -248,14 +244,15 @@
                 <div class="legal flex">
                     <p class="bigger">
                         DurHack follows the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">MLH Code of
-                        Conduct</a>.
+                            Conduct</a>.
                         Photos and videos taken at events in 2019 and 2022.
                     </p>
 
                     <p>
                         DurHack is an event hosted by Durham University Computing Society (<a
-                        href="https://compsoc.tech" target="_blank">compsoc.tech</a>), which is a student society
-                        affiliated with Durham Students' Union (<a href="https://durhamsu.com" target="_blank">durhamsu.com</a>).
+                            href="https://compsoc.tech" target="_blank">compsoc.tech</a>), which is a student society
+                        affiliated with Durham Students' Union (<a href="https://durhamsu.com"
+                            target="_blank">durhamsu.com</a>).
                         Durham Students' Union is registered in England as a company limited by guarantee (07689815) and
                         a charity (1145400), with VAT number 119733690 and registered office Dunelm House, New Elvet,
                         Durham DH1 3AN.
@@ -263,8 +260,7 @@
                 </div>
 
                 <div class="photo">
-                    <img
-                        alt="The text 2022 on its side and then DurHack written in a white outline font"
+                    <img alt="The text 2022 on its side and then DurHack written in a white outline font"
                         src="assets/logo-white-22.png">
                 </div>
             </div>
@@ -279,7 +275,7 @@ import Team from "../components/TeamGrid";
 import Sponsors from "../components/Sponsors";
 
 export default {
-    components: {FAQs, Team, Sponsors},
+    components: { FAQs, Team, Sponsors },
 
 }
 </script>
