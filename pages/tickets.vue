d973f82fe0d45a3eef39828bb02318e187e9ce2e
DCK <devck@protonmail.com>
2022-09-28 10:12:22 +0100

    bodge fix 2

--- pages/tickets.vue
+++ pages/tickets.vue
@@ -1,1 +1,17 @@
-<meta http-equiv="Refresh" content="0; url='https://www.w3docs.com'" />
+
+<template>
+    <meta http-equiv="Refresh" content="0; url='https://www.w3docs.com'" />
+
+    <div class="not-found">
+        <p>Aw no.</p>
+
+        <h1>404</h1>
+
+        <p>We can't find what you're looking for.</p>
+
+        <p>If you think this is a mistake, let us know. <a href="mailto:hello@durhack.com">hello@durhack.com</a></p>
+
+        <a href="/">Take me home</a>
+    </div>
+</template>
+
