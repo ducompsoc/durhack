686403d935e017f10acc5a1a94baf5395bd10d3b
Will Woodward <will@woodwardweb.com>
2023-09-22 20:58:35 +0100

    changed urls to work on build

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -25,32 +25,6 @@
 			</div>
 		</Transition>
 	</div>
-
-	<!-- <div class="timeline" id="small">
-		<div class="group" v-for="(day, key) in schedule">
-			<div v-if="key" class="bracket center column">
-				<div class="day">{{day.day}}</div>
-			</div>
-
-			<div class="row set">
-				<div v-for="event in day.events" class="event">
-					<div class="line row center">
-						<div class="gap"></div>
-					</div>
-
-					<div class="inner">
-						<div class="time">{{event.start}}<span v-if="event.end"> &ndash; {{event.end}}</span></div>
-						<div class="row center">
-							<div class="icon">
-								<img :src="`/assets/icons/${event.icon}.svg`" />
-							</div>
-						</div>
-						<div class="title">{{event.title}}</div>
-					</div>
-				</div>
-			</div>
-        </div>
-	</div> -->
 </template>
 
 <script>
