fd0a303ae3c499071547c147eb8acd94d515363c
Will Woodward <will@woodwardweb.com>
2023-11-13 15:40:36 +0000

    changed intro text

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,10 +1,9 @@
 <template>
     <div id="intro-section">
         <div id="about">
-            <p>We hope you'll join us for the eighth iteration of Durhack, Durham University Computing Society's annual flagship hackathon.</p>
+            <p>Thank you for joining us at the eighth iteration of Durhack, Durham University Computing Society's annual flagship hackathon.</p>
             <h4>4 - 5 November 2023</h4>
-            <p>Students of all experience levels will come together in teams of up to four, helping and learning from each other while creating their own project.</p>
-            <p>Aim to arrive at 9:30-10:30am on the 4th, and we will aim to finish at 16:30pm on the 5th.</p>
+            <p>Students of all experience levels came together in teams of up to four, helping and learning from each other while creating their own project.</p>
         </div>
 
         <div class="row wrap">
