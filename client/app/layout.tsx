8bf7739371aba255e4373cee11d2fc26979595f9
Will Woodward <will@woodwardweb.com>
2024-06-10 21:08:13 +0100

    Initialised project for 2024

---
+++ client/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Durhack",
+  description: "Durham University Computing Society presents the 9th version of Durhack!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <p>
+        Hello World!
+      </p>
+    </main>
+  );
+}
