4d626ab71fa93548d8bf9b658628d56b3f51aa06
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-21 03:51:22 +0100

    wireframe register-interest sub-app

--- server/src/routes/register-interest.ts
+++ server/src/routes/register-interest.ts
@@ -4,3 +4,11 @@ import { cors } from "@tinyhttp/cors"
 import { methodNotAllowed } from "@/middleware/method-not-allowed"
 
 export const registerInterestRoutesApp = new App()
+
+registerInterestRoutesApp
+  .route("/")
+    .all(methodNotAllowed(["OPTIONS", "POST"]))
+    .options(cors())
+    .post(async function (req, res): Promise<void> {
+      res.sendStatus(501)
+    })
