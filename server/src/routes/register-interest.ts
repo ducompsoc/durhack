f5c4d11a9b0007d36546c8156f2d797fce0c82d0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-21 04:37:59 +0100

    handle POST requests by creating record (when possible)

--- server/src/routes/register-interest.ts
+++ server/src/routes/register-interest.ts
@@ -1,14 +1,28 @@
 import { App } from "@tinyhttp/app"
 import { cors } from "@tinyhttp/cors"
+import { z } from "zod"
+import { json } from "milliparsec"
 
+import { prisma } from "@/lib/prisma-client-instance";
 import { methodNotAllowed } from "@/middleware/method-not-allowed"
 
 export const registerInterestRoutesApp = new App()
+
+const registerInterestFormSchema = z.object({
+  firstNames: z.string().trim().min(1),
+  lastNames: z.string().trim().min(1),
+  email: z.string().email(),
+})
 
 registerInterestRoutesApp
   .route("/")
     .all(methodNotAllowed(["OPTIONS", "POST"]))
     .options(cors())
+    .post(json())
     .post(async function (req, res): Promise<void> {
-      res.sendStatus(501)
+      const validatedPayload = registerInterestFormSchema.parse(req.body)
+      await prisma.interest.create({
+        data: validatedPayload
+      })
+      res.sendStatus(204)
     })
