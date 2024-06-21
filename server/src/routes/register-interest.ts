3d97b2595b1b2b603f7c8a804ae9ce7a62fc000f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-21 04:43:10 +0100

    do some error handling perhaps

--- server/src/routes/register-interest.ts
+++ server/src/routes/register-interest.ts
@@ -1,7 +1,8 @@
 import { App } from "@tinyhttp/app"
 import { cors } from "@tinyhttp/cors"
-import { z } from "zod"
+import { z, ZodError } from "zod"
 import { json } from "milliparsec"
+import { Prisma } from "@prisma/client"
 
 import { prisma } from "@/lib/prisma-client-instance";
 import { methodNotAllowed } from "@/middleware/method-not-allowed"
@@ -20,9 +21,23 @@ registerInterestRoutesApp
     .options(cors())
     .post(json())
     .post(async function (req, res): Promise<void> {
-      const validatedPayload = registerInterestFormSchema.parse(req.body)
-      await prisma.interest.create({
-        data: validatedPayload
-      })
-      res.sendStatus(204)
+      try {
+        const validatedPayload = registerInterestFormSchema.parse(req.body)
+        await prisma.interest.create({
+          data: validatedPayload
+        })
+        res.sendStatus(204)
+      } catch (error) {
+        if (error instanceof ZodError) {
+          res.sendStatus(400) // refuse to elaborate hehe
+          return;
+        }
+
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          if (error.code === "P2002") { // https://www.prisma.io/docs/orm/reference/error-reference#p2002
+            res.sendStatus(409)
+            return;
+          }
+        }
+      }
     })
