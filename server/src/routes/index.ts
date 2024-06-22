1309114223aed6e76a67c0cab7d76eec125e4d19
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-22 02:14:45 +0100

    remove verified-schools routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { App } from "@tinyhttp/app";
 
-import { verifiedSchoolsRoutesApp } from "@/routes/verified-schools";
 import { registerInterestRoutesApp } from "@/routes/register-interest"
 
 export const routesApp = new App()
 
-routesApp.use("verified-schools", verifiedSchoolsRoutesApp)
 routesApp.use("register-interest", registerInterestRoutesApp)
