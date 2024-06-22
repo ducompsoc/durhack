98b8fae9b0eae29788e2adfb24190543cf42d563
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-22 02:21:02 +0100

    pass option lists to register form from server component

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,9 +1,24 @@
-import { RegisterForm } from "@/app/register/register-form";
+import { getCountryDataList } from "countries-list";
+import countriesEmoji from 'countries-list/minimal/countries.emoji.min.json'
 
-export default function RegisterPage() {
+import { RegisterForm } from "@/app/register/register-form";
+import { getVerifiedSchoolsList } from "@/data/verified-schools";
+
+export default async function RegisterPage() {
+  const schoolOptions = (await getVerifiedSchoolsList()).map(schoolName => ({
+    label: schoolName,
+    value: schoolName,
+  }))
+
+  const countryOptions = getCountryDataList().map(country => ({
+    label: country.name,
+    emoji: countriesEmoji[country.iso2],
+    value: country.iso3,
+  }))
+
   return (
     <main className="container">
-      <RegisterForm />
+      <RegisterForm schoolOptions={schoolOptions} countryOptions={countryOptions} />
     </main>
   )
 }
