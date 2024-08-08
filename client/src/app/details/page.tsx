0c0acb4ec217a6eb77d62652a74dfc91d3b748e1
Will Woodward <will@woodwardweb.com>
2024-08-08 20:32:55 +0100

    Started main form

--- client/src/app/details/page.tsx
+++ client/src/app/details/page.tsx
@@ -1,9 +1,42 @@
 import { Sidebar } from "@/app/details/sidebar";
+import Header from "@/app/details/header";
+
+import { Input } from "@durhack/web-components/ui/input"
+import { Label } from "@durhack/web-components/ui/label";
+
+type Profile = {
+    firstNames: string
+    lastNames: string
+    age: string
+    phoneNumber: string
+    email: string
+    school: string
+    graduationYear: string
+    levelOfStudy: string
+    countryOfResidence: string
+}
 
 export default async function DetailsPage() {
-  return (
-    <main className="">
-        <Sidebar />
-    </main>
-  )
+    const mockProfile: Profile = {
+        firstNames: "Will",
+        lastNames: "Woodward",
+        age: "20",
+        phoneNumber: "phone number",
+        email: "Will's email",
+        school: "Durham",
+        graduationYear: "2026",
+        levelOfStudy: "Level 3",
+        countryOfResidence: "United Kingdom"
+    }
+
+    return (
+        <main className="min-h-[100vh]">
+            <Header />
+            <Sidebar />
+            <div className="ml-64 py-16 pl-16 pr-64">
+                <Label>First Name</Label>
+                <Input placeholder={ mockProfile.firstNames }/>
+            </div>
+        </main>
+    )
 }
--- client/src/app/details/sidebar.tsx
+++ client/src/app/details/sidebar.tsx
@@ -8,17 +8,17 @@ export function Sidebar() {
 
     const menuItems: menuItem[] = [
         {name: 'Home', link: '/'},
-        {name: 'Tab2', link: '/'},
+        {name: 'Accounts', link: '/'},
         {name: 'Tab3', link: '/'}
     ];
 
     return (
-        <div className="px-2 my-2 border-r-2 w-64 min-h-[100vh]">
+        <div className="px-2 my-2 w-64 absolute">
             <div className="flex flex-col">
             {
                 menuItems.map((item) => {
                     return (
-                        <a href={'details/' + item.link} className="py-1 px-2 border-x-2 border-transparent rounded-lg hover:border-gray-100 hover:bg-white hover:bg-opacity-15">
+                        <a href={'details/' + item.link} className="py-1 mt-1 px-2 border-x-2 rounded-lg hover:border-white  bg-white bg-opacity-5 hover:bg-opacity-15 hover:transition-all duration-300">
                             <p className="text-xl font-medium">{ item.name }</p>
                         </a>
                     )
