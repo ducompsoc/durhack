497877a8a2c9124ec01ef5980bbf84155c24e492
Will Woodward <will@woodwardweb.com>
2024-08-27 09:58:18 +0100

    Separated components

--- client/src/app/details/page.tsx
+++ client/src/app/details/page.tsx
@@ -7,9 +7,23 @@ import Header from "@/app/details/header";
 
 import { Input } from "@durhack/web-components/ui/input"
 import { Label } from "@durhack/web-components/ui/label";
-import { LinkBox } from "./linkbox";
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormControl,
+    FormLabel,
+    FormMessage,
+    FormDescription,
+} from "@durhack/web-components/ui/form"
+import Link from "next/link"
+import { Button } from "@durhack/web-components/ui/button"
+import { Checkbox } from "@durhack/web-components/ui/checkbox"
 
-type Profile = {
+import { LinkBox } from "@/app/details/linkbox";
+import HomePage from "@/app/details/homepage";
+
+export type Profile = {
     firstNames: string
     lastNames: string
     age: string
@@ -46,40 +60,14 @@ export default function DetailsPage() {
             <Sidebar selectPage={selectPage}/>
             <div className="ml-64 py-16 pl-16 pr-64">
                 { page == "home" &&
-                    <>
-                        <h2 className="text-2xl">
-                            Details
-                        </h2>
-                        <div className="columns-2 mb-4">
-                            <Label>First Name</Label>
-                            <Input placeholder={ mockProfile.firstNames }/>
-                            <Label>Last Name</Label>
-                            <Input placeholder={ mockProfile.lastNames }/>
-                        </div>
-                        <div className="columns-2 mb-4">
-                            <Label>Phone Number</Label>
-                            <Input placeholder={ mockProfile.phoneNumber }/>
-                            <Label>Email</Label>
-                            <Input placeholder={ mockProfile.email }/>
-                        </div>
-                        <div className="mb-8">
-                            <Label>Email</Label>
-                            <Input placeholder={ mockProfile.email }/>
-                        </div>
-
-                        <h2 className="text-2xl">
-                            Authentication
-                        </h2>
-                        <LinkBox links={["Keycloak"]} />
-                        <div className="mt-16">
-                            <div className="mx-[45%] py-2 px-4 text-center rounded-sm bg-white bg-opacity-15 hover:bg-green-500 hover:cursor-pointer hover:shadow-[0_0px_50px_0px_rgba(34,197,94,0.8)] transition-all">
-                                Submit
-                            </div>
-                        </div>
-                    </>
+                    HomePage(mockProfile)
                 }
                 { page == "accounts" &&
                     <LinkBox links={["GitHub", "LinkedIn"]} />
+                }
+                { page == "complete" && 
+                    <>
+                    </>
                 }
             </div>
         </main>
