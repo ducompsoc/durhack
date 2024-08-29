070defa5e8853943e5a15e8b660047890a27437c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-29 01:15:09 +0100

    add `themeColor` to site config

--- client/src/config/site.ts
+++ client/src/config/site.ts
@@ -2,6 +2,7 @@ export const siteConfig = {
   name: "DurHack",
   url: "https://durhack.com",
   apiUrl: "https://api.durhack.com",
+  themeColor: "#1d1934",
   description: "DurHack is an annual hackathon event hosted by Durham University Computing Society (compsoc.tech), which is a student society affiliated with Durham Students' Union.",
   links: {
     "github": "https://github.com/ducompsoc"
