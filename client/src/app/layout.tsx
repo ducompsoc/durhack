1fa2845bb5fb2db5d7a54a6e4f6fa9e481479c5b
Will Woodward <will@woodwardweb.com>
2024-09-03 20:55:04 +0100

    Added navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Space_Grotesk } from "next/font/google"
 
 import "@/styles/globals.css";
 import { siteConfig } from "@/config/site";
-import { Footer } from "@/components/footer"
+import { Footer } from "@/components/footer";
+import Navbar from "@/components/navbar";
 import { cn } from "@/lib/utils";
 
 const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
@@ -32,13 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className={cn(space_grotesk.className, "dark antialiased")}>
-      <a id="mlh-trust-badge"
-         style={{display: "block", maxWidth: "100px", minWidth: "60px", position: "absolute", right: "50px", top: "0", width: "10%", zIndex: "10000"}}
-         href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
-         target="_blank">
-        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg"
-          alt="Major League Hacking 2025 Hackathon Season" className="w-full"/>
-      </a>
+      <div className="fixed top-0 overflow-visible w-[100%] z-50">
+        <a id="mlh-trust-badge"
+          style={{display: "block", maxWidth: "100px", minWidth: "60px", position: "absolute", right: "50px", top: "0", width: "10%", zIndex: "10000"}}
+          href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
+          target="_blank">
+          <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg"
+            alt="Major League Hacking 2025 Hackathon Season" className="w-full"/>
+        </a>
+        <Navbar />
+      </div>
       <div className="relative bg-background">
         {children}
         <Footer />
