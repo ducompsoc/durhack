8ede08e46bd12afa5c5bb14b3c034d5479835140
Will Woodward <will@woodwardweb.com>
2024-09-04 00:48:31 +0100

    Added moon

--- client/src/components/splash.tsx
+++ client/src/components/splash.tsx
@@ -1,5 +1,7 @@
 export default function Splash() {
     return (
-        <></>
+        <div className="h-[100vh]">
+            <img src="/assets/icons/moon.png" alt="splash" className="h-[32rem] w-[32rem] top-[-4rem] mx-auto rounded-[50%] transition-all duration-500 hover:transition-all hover:duration-300 relative" id="moon"/>
+        </div>
     )
 }
