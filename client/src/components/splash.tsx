07e7a41f19a5099431bc8d7dd0eca9c2b170b33e
Will Woodward <will@woodwardweb.com>
2024-09-04 01:03:39 +0100

    Added outer circles

--- client/src/components/splash.tsx
+++ client/src/components/splash.tsx
@@ -1,7 +1,9 @@
 export default function Splash() {
     return (
-        <div className="h-[100vh]">
-            <img src="/assets/icons/moon.png" alt="splash" className="h-[32rem] w-[32rem] top-[-4rem] mx-auto rounded-[50%] transition-all duration-500 hover:transition-all hover:duration-300 relative" id="moon"/>
+        <div className="h-[3000px]">
+            <div id="outer-ring" className="rounded-[50%] absolute h-[94rem] w-[94rem] top-[-35rem]"></div>
+            <div id="inner-ring" className="rounded-[50%] absolute h-[56rem] w-[56rem] top-[-16rem]"></div>
+            <img src="/assets/icons/moon.png" alt="splash" className="h-[32rem] w-[32rem] top-[-4rem] mx-auto rounded-[50%] transition-all duration-500 hover:transition-all hover:duration-300 absolute" id="moon"/>
         </div>
     )
 }
