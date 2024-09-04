4ebd7cfdf812f67e59b03cd34f13cfda0c2d3a6b
Will Woodward <will@woodwardweb.com>
2024-09-04 13:10:23 +0100

    Added clouds and animations

--- client/src/components/splash.tsx
+++ client/src/components/splash.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import * as React from "react";
 
 export default function Splash() {
@@ -10,19 +10,38 @@ export default function Splash() {
         target: ref,
         offset: ["start start", "end start"],
     });
-    const cloud1 = useTransform(scrollYProgress, [0, 1], ["0rem", "100rem"]);
+    const springProgress = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
+
+    const cloud1X = useTransform(springProgress, [0, 1], ["0rem", "200rem"]);
+    const cloud1Y = useTransform(scrollYProgress, [0, 1], ["0rem", "0rem"]);
+    const cloud2X = useTransform(springProgress, [0, 1], ["0rem", "-200rem"]);
+    const cloud2Y = useTransform(scrollYProgress, [0, 1], ["0rem", "0rem"]);
+    const cloud3X = useTransform(springProgress, [0, 1], ["0rem", "-300rem"]);
+    const cloud3Y = useTransform(scrollYProgress, [0, 1], ["0rem", "0rem"]);
 
     return (
-        <div ref={ref} className="">
+        <div ref={ref} className="overflow-hidden relative">
             <div id="outer-ring" className="rounded-[50%] absolute h-[94rem] w-[94rem] top-[-35rem]"></div>
             <div id="inner-ring" className="rounded-[50%] absolute h-[56rem] w-[56rem] top-[-16rem]"></div>
             <img src="/assets/icons/moon.png" alt="splash" className="h-[32rem] w-[32rem] top-[-4rem] mx-auto rounded-[50%] transition-all duration-500 hover:transition-all hover:duration-300 absolute" id="moon"/>
         
-            <motion.div id="cloud-1" className="absolute right-[-10rem] top-[16rem]" style={{ x: cloud1 }}>
+            <motion.div id="cloud-1" className="absolute right-[-10rem] top-[16rem]" style={{ x: cloud1X, y: cloud1Y }}>
                 <img src="/assets/icons/cloud-1.png" alt="cloud" className="" />
             </motion.div>
 
-            <div className="h-[300vh]"></div>
+            <motion.div id="cloud-2" className="absolute left-[-20rem] top-[16rem]" style={{ x: cloud2X, y: cloud2Y }}>
+                <img src="/assets/icons/cloud-2.png" alt="cloud" className="" />
+            </motion.div>
+
+            <motion.div id="cloud-3" className="absolute left-[-5rem] top-[26rem]" style={{ x: cloud3X, y: cloud3Y }}>
+                <img src="/assets/icons/cloud-3.png" alt="cloud" className="" />
+            </motion.div>
+
+            <div className="h-[200vh]">
+                <div>
+                    <h2></h2>
+                </div>
+            </div>
             <div>
                 Test div
             </div>
