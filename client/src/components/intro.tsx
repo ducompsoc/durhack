a5ad2695fdfeb7f973347d9ee3c2820552c580c4
Will Woodward <will@woodwardweb.com>
2024-09-13 19:57:31 +0100

    Started background for intro

--- client/src/components/intro.tsx
+++ client/src/components/intro.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import * as React from "react";
+
 export default function Intro() {
+    const ref = React.useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"],
+    });
+    const springProgress = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
+
     return (
-        <></>
+        <div ref={ref} className="relative">
+            <div id="cloud-4" className="absolute left-[-10rem] top-[30rem] z-30 pointer-events-none">
+                <img src="/assets/icons/cloud-4.svg" alt="cloud" className="h-[6rem] xl:h-[20rem]" />
+            </div>
+
+            <div id="cloud-5" className="absolute right-[-7rem] top-[50rem] z-30 pointer-events-none">
+                <img src="/assets/icons/cloud-5.svg" alt="cloud" className="h-[6rem] xl:h-[20rem]" />
+            </div>
+        </div>
     )
 }
