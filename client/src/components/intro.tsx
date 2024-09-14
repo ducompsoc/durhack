9667e3cd9e1bd9d0b7e377070b0a7e33732d4c39
Will Woodward <will@woodwardweb.com>
2024-09-14 12:24:21 +0100

    Mobile responsive intro section

--- client/src/components/intro.tsx
+++ client/src/components/intro.tsx
@@ -20,15 +20,15 @@ export default function Intro() {
         <>
             <div ref={ref} className="relative">
                 <motion.div id="cloud-4" className="absolute left-[-10rem] top-[40rem] z-30 pointer-events-none" style={{ x: cloud4X }}>
-                    <img src="/assets/icons/cloud-4.svg" alt="cloud" className="h-[6rem] xl:h-[20rem]" />
+                    <img src="/assets/icons/cloud-4.svg" alt="cloud" className="h-[12rem] lg:h-[20rem]" />
                 </motion.div>
 
                 <motion.div id="cloud-5" className="absolute right-[-7rem] top-[60rem] z-30 pointer-events-none" style={{ x: cloud5X }}>
-                    <img src="/assets/icons/cloud-5.svg" alt="cloud" className="h-[6rem] xl:h-[20rem]" />
+                    <img src="/assets/icons/cloud-5.svg" alt="cloud" className="h-[12rem] lg:h-[20rem]" />
                 </motion.div>
             </div>
 
-            <div className="absolute mt-[22rem] ml-[7rem] text-4xl w-[40rem]">
+            <div className="absolute mt-[22rem] mx-[12.5vw] text-3xl md:text-4xl w-[75vw]">
                 We welcome you to join us for the ninth iteration of DurHack, Durham University Computing Society's annual flagship hackathon.
             </div>
         </>
