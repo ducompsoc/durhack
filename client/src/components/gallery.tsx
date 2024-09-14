2d05861d65f47c720a19f42902ada9b57351eebc
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-14 21:05:52 +0100

    ensure slides' images are centered appropriately and slide dimensions match

--- client/src/components/gallery.tsx
+++ client/src/components/gallery.tsx
@@ -19,28 +19,31 @@ export default function Gallery() {
         <div className="w-full max-w-[60rem]">
           <Carousel opts={{ align: "start", direction: "ltr", loop: true }} orientation="vertical" className="my-20">
             <CarouselContent className="h-[60vw] max-h-[40rem]">
-              <CarouselItem>
+              <CarouselItem className="h-[inherit] max-h-[inherit]">
                 <Image
                   src="/assets/photos/anders-jilden-uwbajDCODj4-unsplash.jpg"
                   alt="golden hour photograph of docking pier on body of water"
                   width={2400}
                   height={1600}
+                  className="h-full object-cover"
                 />
               </CarouselItem>
-              <CarouselItem>
+              <CarouselItem className="h-[inherit] max-h-[inherit]">
                 <Image
                   src="/assets/photos/daniel-leone-g30P1zcOzXo-unsplash.jpg"
                   alt="photograph of a snowy mountain before sunrise"
                   width={4928}
                   height={3264}
+                  className="h-full object-cover"
                 />
               </CarouselItem>
-              <CarouselItem>
+              <CarouselItem className="h-[inherit] max-h-[inherit]">
                 <Image
                   src="/assets/photos/qingbao-meng-01_igFr7hd4-unsplash.jpg"
                   alt="bird's eye view photograph of green mountains"
                   width={2600}
                   height={1548}
+                  className="h-full object-cover"
                 />
               </CarouselItem>
             </CarouselContent>
