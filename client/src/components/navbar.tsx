afbe453acd149ed261ae74a00536766e0831193f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-16 14:05:26 +0100

    use `text-transform` instead of plain uppercase strings

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -8,18 +8,18 @@ export default function Navbar() {
     <nav className={cn(audiowide.className, "hidden md:flex px-16 py-5 mx-auto relative")}>
       <a href="/" className="flex pr-32 space-x-4 hover:cursor-pointer absolute">
         <img src="/assets/icons/logo.svg" className="w-16 h-16" />
-        <div className="text-2xl pt-3.5 hidden xl:block" id="title">
-          DURHACK
+        <div className="text-2xl pt-3.5 hidden xl:block uppercase" id="title">
+          DurHack
         </div>
       </a>
 
-      <ul className="flex py-5 pl-16 space-x-8 lg:space-x-16 mx-auto">
-        <li>HOME</li>
-        <li>ABOUT</li>
+      <ul className="flex py-5 pl-16 space-x-8 lg:space-x-16 mx-auto *:uppercase">
+        <li>Home</li>
+        <li>About</li>
         <li>FAQs</li>
-        <li>SPONSORS</li>
-        <li>SCHEDULE</li>
-        <li>TEAM</li>
+        <li>Sponsors</li>
+        <li>Schedule</li>
+        <li>Team</li>
       </ul>
     </nav>
   );
