9bba9310a4c05c18a3f9dd18237eb7732cc7fd27
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-18 18:17:13 +0100

    use own component for MLH banner

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Space_Grotesk } from "next/font/google"
 import "@/styles/globals.css";
 import { siteConfig } from "@/config/site";
 import { Footer } from "@/components/footer";
+import { MLHBanner } from "@/components/mlh-banner";
 import Navbar from "@/components/navbar";
 import { cn } from "@/lib/utils";
 
@@ -37,8 +38,7 @@ export default function RootLayout({
           style={{maxWidth: "100px", minWidth: "60px", position: "absolute", right: "50px", top: "0", width: "10%", zIndex: "10000"}}
           href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
           target="_blank">
-          <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg"
-            alt="Major League Hacking 2025 Hackathon Season" className="w-full"/>
+          <MLHBanner variant="white" className="w-full" />
         </a>
         <Navbar />
       </div>
