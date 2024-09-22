df0f02ebee6a3c80ff2563c9993704e3eb183199
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-22 15:26:20 +0100

    only use one react icons library

--- client/src/components/details/header.tsx
+++ client/src/components/details/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react";
-import { FaBars } from 'react-icons/fa';
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import { SidebarContext } from "@/app/details/layout";
 
 export default function Header() {
@@ -11,7 +11,7 @@ export default function Header() {
         <div className="px-8 py-8 border-b-2 h-32 flex row">
             <div className="pr-8 h-full content-center md:hidden">
                 <div className="p-3 rounded-md bg-white bg-opacity-20" onClick={() => (toggleSidebar())}>
-                    <FaBars />
+                    <HamburgerMenuIcon />
                 </div>
             </div>
             <div>
