5e7e2f93a025a191da0f7d312f50a596c7d5cf41
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-29 18:58:42 +0100

    prefer `throw`ing a recognisable error to `return null`

--- client/src/hooks/use-application.ts
+++ client/src/hooks/use-application.ts
@@ -1,20 +1,21 @@
 import useSWR from "swr"
 import type { Application } from "@durhack/durhack-common/types/application"
+import ModuleError from "module-error"
 
 import { siteConfig } from "@/config/site"
 
 export type { Application }
 
-async function applicationFetcher(path: string): Promise<Application | null> {
+async function applicationFetcher(path: string): Promise<Application> {
   const url = new URL(path, siteConfig.apiUrl).toString()
   const response = await fetch(url, { credentials: "include" })
 
-  if (response.status === 401) return null
-  if (!response.ok) throw new Error("Couldn't fetch application!")
+  if (response.status === 401) throw new ModuleError("Couldn't fetch user registration details because user is not logged in", { code: "ERR_UNAUTHENTICATED" })
+  if (!response.ok) throw new Error("Couldn't fetch user registration details for unknown reason")
 
   return (await response.json()).data as Application
 }
 
 export function useApplication() {
-  return useSWR<Application | null, unknown | undefined>("/application", applicationFetcher)
+  return useSWR<Application, unknown | undefined>("/application", applicationFetcher)
 }
