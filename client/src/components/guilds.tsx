9ae727ab1cab0a925425d05ed8e0c60d174a635d
Will Woodward <will@woodwardweb.com>
2024-09-30 12:10:38 +0100

    fixed guilds responsiveness

--- client/src/components/guilds.tsx
+++ client/src/components/guilds.tsx
@@ -8,15 +8,15 @@ import { SectionHeader } from "@/components/section-header"
 
 export default function Guilds() {
   return (
-    <div className="text-center px-16 py-[24rem]">
+    <div className="text-center px-8 sm:px-16 py-[24rem]">
       <SectionHeader>Guilds</SectionHeader>
 
-      <div className="text-center text-lg md:text-xl px-4 sm:px-8 md:px-12 lg:px-24">
+      <div className="text-center pt-8 pb-24 text-lg md:text-2xl px-4 sm:px-8 md:px-12 lg:px-24">
         These will put each of the hacking teams into larger groups, where they all can collect points for their guild towards a small prize.
       </div>
 
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-16 w-full">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 sm:gap-16 md:gap-6 lg:gap-16 w-full">
           <GuildCentreOfTheEarthIcon className="w-full h-auto mx-auto drop-shadow-lg" />
           <GuildAtlantisIcon className="w-full h-auto mx-auto drop-shadow-lg" />
           <GuildMoonIcon className="w-full h-auto mx-auto drop-shadow-lg" />
