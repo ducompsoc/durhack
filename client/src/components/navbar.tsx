a9e3aedd54d3e81affee223329fbecef87d8f4bf
Will Woodward <will@woodwardweb.com>
2024-09-30 21:59:37 +0100

    navbar links

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -8,7 +8,7 @@ import "@/styles/navbar.css"
 export default function Navbar() {
   return (
     <nav className={cn(audiowide.className, "hidden md:flex px-16 py-5 mx-auto relative")}>
-      <a href="/" className="flex pr-32 space-x-4 hover:cursor-pointer absolute">
+      <a href="#splash" className="flex pr-32 space-x-4 hover:cursor-pointer absolute">
         <Image
           src="/assets/branding/durhack-2024-mark.svg"
           width={290}
@@ -22,12 +22,24 @@ export default function Navbar() {
       </a>
 
       <ul className="flex py-5 pl-16 space-x-8 lg:space-x-16 mx-auto *:uppercase">
-        <li>Home</li>
-        <li>About</li>
-        <li>FAQs</li>
-        <li>Sponsors</li>
-        <li>Schedule</li>
-        <li>Team</li>
+        <li>
+          <a href="#splash">Home</a>
+        </li>
+        <li>
+          <a href="#intro">About</a>
+        </li>
+        <li>
+          <a href="#sponsors">FAQs</a>
+        </li>
+        <li>
+          <a href="#sponsors">Sponsors</a>
+        </li>
+        <li>
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li>
+          <a href="#end">Team</a>
+        </li>
       </ul>
     </nav>
   )
