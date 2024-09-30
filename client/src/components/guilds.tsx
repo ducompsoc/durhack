e743d8d55b0aba81ac8a9f550cf6cade73496698
Will Woodward <will@woodwardweb.com>
2024-09-30 12:00:30 +0100

    Guild section fixes

--- client/src/components/guilds.tsx
+++ client/src/components/guilds.tsx
@@ -8,19 +8,19 @@ import { SectionHeader } from "@/components/section-header"
 
 export default function Guilds() {
   return (
-    <div className="text-center px-16">
+    <div className="text-center px-16 py-[24rem]">
       <SectionHeader>Guilds</SectionHeader>
 
       <div className="text-center text-lg md:text-xl px-4 sm:px-8 md:px-12 lg:px-24">
-        Guilds text goes here. needs to be rewritten for 2024
+        These will put each of the hacking teams into larger groups, where they all can collect points for their guild towards a small prize.
       </div>
 
       <div className="container mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-16 w-full">
-          <GuildCentreOfTheEarthIcon className="w-full h-auto mx-auto shadow-md" />
-          <GuildAtlantisIcon className="w-full h-auto mx-auto shadow-md" />
-          <GuildMoonIcon className="w-full h-auto mx-auto shadow-md" />
-          <GuildMysteriousIslandIcon className="w-full h-auto mx-auto shadow-md" />
+          <GuildCentreOfTheEarthIcon className="w-full h-auto mx-auto drop-shadow-lg" />
+          <GuildAtlantisIcon className="w-full h-auto mx-auto drop-shadow-lg" />
+          <GuildMoonIcon className="w-full h-auto mx-auto drop-shadow-lg" />
+          <GuildMysteriousIslandIcon className="w-full h-auto mx-auto drop-shadow-lg" />
         </div>
       </div>
     </div>
