25d6b26944e091ca3f924a49cbbe5a6241d596eb
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-03 20:18:00 +0100

    don't use non-standard `NODE_ENV` variable value

--- client/src/lib/environment.ts
+++ client/src/lib/environment.ts
@@ -1,4 +1,11 @@
-export const environment = process.env.NODE_ENV as string
+type Environment = "staging" | "production" | "development" | "test"
+
+function resolveEnvironment(): Environment {
+  if (process.env.APP_ENV === "staging") return "staging"
+  return process.env.NODE_ENV
+}
+
+export const environment = resolveEnvironment()
 export const isDevelopment = environment === "development"
 export const isProduction = environment === "production"
 export const isStaging = environment === "staging"
