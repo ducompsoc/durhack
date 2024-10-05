ee287d9a645c0f8f0b0a6ddb9145504818c74b11
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-05 14:55:15 +0100

    passthrough props in `SectionHeader`

--- client/src/components/section-header.tsx
+++ client/src/components/section-header.tsx
@@ -4,12 +4,15 @@ import { audiowide } from "@/lib/google-fonts"
 import { cn } from "@/lib/utils"
 import "@/styles/section-header.css"
 
-export function SectionHeader({ children }: { children: React.ReactNode }) {
+export function SectionHeader({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
   return (
     <h1
-      className={cn(audiowide.className, "section-header uppercase text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-bold w-fit")}
-    >
-      {children}
-    </h1>
+      className={cn(
+        audiowide.className,
+        "section-header uppercase text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-bold w-fit",
+        className,
+      )}
+      {...props}
+    />
   )
 }
