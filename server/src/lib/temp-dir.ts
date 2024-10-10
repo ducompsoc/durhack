475d39191940d6722ed9bd962e88423af8b0b2df
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-10 04:09:55 +0100

    fix: invoke `realpath` on a path that exists

--- server/src/lib/temp-dir.ts
+++ server/src/lib/temp-dir.ts
@@ -1,6 +1,6 @@
 import { tmpdir } from "node:os"
 import { mkdir, realpath } from "node:fs/promises"
-import { resolve as pathResolve, sep } from "node:path"
+import { join as pathJoin, sep } from "node:path"
 import { nanoid } from "nanoid/non-secure"
 
 function generateApplicationInstanceTempDirName(): string {
@@ -9,7 +9,7 @@ function generateApplicationInstanceTempDirName(): string {
 
 export async function getTempDir(): Promise<string> {
   const applicationInstanceTempDirName = generateApplicationInstanceTempDirName()
-  const applicationInstanceTempDirPath = await realpath(pathResolve(tmpdir(), applicationInstanceTempDirName))
+  const applicationInstanceTempDirPath = pathJoin(await realpath(tmpdir()), applicationInstanceTempDirName)
   await mkdir(applicationInstanceTempDirPath, 0o700)
   return applicationInstanceTempDirPath
 }
