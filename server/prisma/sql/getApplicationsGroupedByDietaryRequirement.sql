9ba375a5ac143c4f3e0e559fe9301adf60c429da
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-16 18:19:54 +0100

    fix: update typedSql queries to match changes to database schema

--- server/prisma/sql/getApplicationsGroupedByDietaryRequirement.sql
+++ server/prisma/sql/getApplicationsGroupedByDietaryRequirement.sql
@@ -1,10 +1,10 @@
 SELECT
-    SUBSTRING(userFlag."flagName" from 21) as "dietaryRequirement",
+    SUBSTRING(userFlag.flag_name from 21) as "dietaryRequirement",
     COUNT(userInfo.user_id) as "count"
 FROM "UserInfo" userInfo
 LEFT JOIN "UserFlag" userFlag on userInfo.user_id = userFlag.user_id
 WHERE true
     AND application_status='submitted'
-    AND userFlag."flagName" like 'dietary-requirement:%'
+    AND userFlag.flag_name like 'dietary-requirement:%'
 GROUP BY
-    userFlag."flagName"
+    userFlag.flag_name
--- server/prisma/sql/getApplicationsGroupedByDisciplineOfStudy.sql
+++ server/prisma/sql/getApplicationsGroupedByDisciplineOfStudy.sql
@@ -1,10 +1,10 @@
 SELECT
-    SUBSTRING(userFlag."flagName" from 21) as "disciplineOfStudy",
+    SUBSTRING(userFlag.flag_name from 21) as "disciplineOfStudy",
     COUNT(userInfo.user_id) as "count"
 FROM "UserInfo" userInfo
 LEFT JOIN "UserFlag" userFlag on userInfo.user_id = userFlag.user_id
 WHERE true
     AND application_status='submitted'
-    AND userFlag."flagName" like 'discipline-of-study:%'
+    AND userFlag.flag_name like 'discipline-of-study:%'
 GROUP BY
-    userFlag."flagName"
+    userFlag.flag_name
