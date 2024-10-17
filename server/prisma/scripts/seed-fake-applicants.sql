a6e53a041817440ae3adc8f15a0800b0e3e6294a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-17 20:46:15 +0100

    seed ages

--- server/prisma/scripts/seed-fake-applicants.sql
+++ server/prisma/scripts/seed-fake-applicants.sql
@@ -17,12 +17,20 @@ SELECT
 FROM generate_series(1, 1200);
 
 -- For each 'user' create a submitted application
-INSERT INTO "UserInfo" (user_id, application_status, application_submitted_at, updated_at)
+INSERT INTO "UserInfo"
+    (user_id, age, application_status, application_submitted_at, updated_at)
 SELECT
     keycloak_user_id,
+    20,
     'submitted',
     random_timestamp(),
     timestamp '2024-07-01 00:00:00'
 FROM "User";
+
+-- Select a random sample of users of size 3% and assign their age to less than 18
+UPDATE "UserInfo" userInfo
+SET age = 17
+FROM (SELECT user_id, age FROM "UserInfo" TABLESAMPLE BERNOULLI(3)) AS sample
+WHERE userInfo.user_id = sample.user_id
 
 -- Now test the `ticket-assigner`: `pnpm exec tsx src/ticket-assigner/main.ts`
