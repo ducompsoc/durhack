1c5ca9f6fb4b71802e11891a13ada3ee79e0d62f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-17 16:24:45 +0100

    add `queryString` to `Request` proto

--- server/src/request.ts
+++ server/src/request.ts
@@ -1,6 +1,7 @@
 import { Request as OtterRequest } from "@otterhttp/app"
 import type { Prisma } from "@prisma/client"
 import type { TokenSet as ClientTokenSet, UserinfoResponse } from "openid-client"
+import { stringify } from "node:querystring";
 
 import type { KeycloakUserInfo } from "@/lib/keycloak-client"
 
@@ -8,6 +9,8 @@ export class Request extends OtterRequest<never> {
   user?: Prisma.UserGetPayload<{ include: { tokenSet: true } }>
   userTokenSet?: ClientTokenSet
   userProfile?: UserinfoResponse<KeycloakUserInfo>
+
+  private _queryString?: string
 
   get origin() {
     return `${this.protocol}://${this.host}`
@@ -20,5 +23,12 @@ export class Request extends OtterRequest<never> {
 
   get href() {
     return `${this.origin}${this.url}`
+  }
+
+  get queryString(): string {
+    if (this._queryString != null) return this._queryString
+    let queryString = stringify(this.query)
+    if (queryString !== "") queryString = `?${queryString}`
+    return this._queryString = queryString
   }
 }
