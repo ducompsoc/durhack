59bf46ae54dcfa1904c3cb7f58d047e210d5491b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-18 00:11:58 +0100

    use the `KeycloakAugmentingTransform` in the pipeline

--- server/src/ticket-assigner/main.ts
+++ server/src/ticket-assigner/main.ts
@@ -6,7 +6,8 @@ import { prisma } from "@/database"
 import { AttendeeCheckingTransform } from "./attendee-checking-transform";
 import { TicketAssigningWritable } from "./ticket-assigning-writable"
 import { generateUserInfoByTicketAssignmentOrder } from "./ticket-order-user-info-async-generator"
-import { MailgunMailer } from "./mailer";
+import { MailgunMailer } from "./mailer"
+import { KeycloakAugmentingTransform } from "./keycloak-augmenting-transform"
 
 const totalAssignedTicketCount = await prisma.userInfo.count({
   where: { applicationStatus: { equals: "accepted" } },
@@ -16,8 +17,14 @@ const mailer = new MailgunMailer()
 
 const userInfoReadable = Readable.from(generateUserInfoByTicketAssignmentOrder())
 const attendeeCheckingTransform = new AttendeeCheckingTransform()
+const userInfoAugmentingTransform = new KeycloakAugmentingTransform()
 const ticketAssigningWritable = new TicketAssigningWritable(mailer, totalAssignedTicketCount)
 
-await pipeline(userInfoReadable, attendeeCheckingTransform, ticketAssigningWritable)
+await pipeline(
+  userInfoReadable,
+  attendeeCheckingTransform,
+  userInfoAugmentingTransform,
+  ticketAssigningWritable,
+)
 const newlyAssignedTicketCount = ticketAssigningWritable.totalAssignedTicketCount - totalAssignedTicketCount
 console.log(`Assigned ${newlyAssignedTicketCount} tickets`)
