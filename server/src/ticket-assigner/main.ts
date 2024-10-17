e2582272bf502a55fd367354a50291336c8887ae
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-17 18:14:11 +0100

    we do a little abstraction

--- server/src/ticket-assigner/main.ts
+++ server/src/ticket-assigner/main.ts
@@ -6,14 +6,17 @@ import { prisma } from "@/database"
 import { AttendeeCheckingTransform } from "./attendee-checking-transform";
 import { TicketAssigningWritable } from "./ticket-assigning-writable"
 import { generateUserInfoByTicketAssignmentOrder } from "./ticket-order-user-info-async-generator"
+import { MailgunMailer } from "./mailer";
 
 const totalAssignedTicketCount = await prisma.userInfo.count({
   where: { applicationStatus: { equals: "accepted" } },
 })
 
+const mailer = new MailgunMailer()
+
 const userInfoReadable = Readable.from(generateUserInfoByTicketAssignmentOrder())
 const attendeeCheckingTransform = new AttendeeCheckingTransform()
-const ticketAssigningWritable = new TicketAssigningWritable(totalAssignedTicketCount)
+const ticketAssigningWritable = new TicketAssigningWritable(mailer, totalAssignedTicketCount)
 
 await pipeline(userInfoReadable, attendeeCheckingTransform, ticketAssigningWritable)
 const newlyAssignedTicketCount = ticketAssigningWritable.totalAssignedTicketCount - totalAssignedTicketCount
