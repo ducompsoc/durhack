126fe42ec85fd2bbbc4ec45dbd314cb98e28f4b8
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-28 22:53:28 +0000

    implement attendance filtering

--- server/src/routes/applications/data-export/data-export-handlers.ts
+++ server/src/routes/applications/data-export/data-export-handlers.ts
@@ -4,19 +4,22 @@ import { rm, mkdir } from "node:fs/promises"
 import { join as pathJoin } from "node:path"
 import { Readable } from "node:stream"
 import { pipeline } from "node:stream/promises"
+import { ServerError } from "@otterhttp/errors"
 
 import { Group, onlyGroups } from "@/decorators/authorise"
 import { KeycloakAugmentingTransform } from "@/lib/keycloak-augmenting-transform"
 import { getTempDir } from "@/lib/temp-dir"
+import { waitForExit } from "@/lib/wait-for-exit"
 import type { Middleware } from "@/types"
+import { UserInfo } from "@/database";
 
+import { AttendanceAugmentingTransform, type AttendanceAugments } from "./attendance-augmenting-transform";
 import { HukCsvTransform } from "./huk-csv-transform"
 import { MlhCsvTransform } from "./mlh-csv-transform"
 import { generateUserInfo } from "./user-info-async-generator"
 import { generateUserCv } from "./user-cv-async-generator"
 import { CvExportingWritable } from "./cv-exporting-writable"
-import {waitForExit} from "@/lib/wait-for-exit";
-import {ServerError} from "@otterhttp/errors";
+import { FilteringTransform } from "./filtering-transform"
 
 class DataExportHandlers {
   /**
@@ -26,18 +29,28 @@ class DataExportHandlers {
   @onlyGroups([Group.organisers, Group.admins])
   getMajorLeagueHacking(): Middleware {
     return async (request, response) => {
+      const attendeesOnly = request.query.attendees != null
       const tempDir = await getTempDir()
       try {
-        const fileDestination = pathJoin(tempDir, "major-league-hacking-data-export.csv") // the name of the temporary file doesn't actually matter
+        const fileName = attendeesOnly
+          ? "major-league-hacking-attendees.csv"
+          : "major-league-hacking-applicants.csv"
+        const fileDestination = pathJoin(tempDir, fileName) // the name of the temporary file doesn't actually matter
+
+        const filterTransform = attendeesOnly
+          ? new FilteringTransform<UserInfo & AttendanceAugments>((item) => item.isCheckedIn)
+          : new FilteringTransform(() => true)
 
         await pipeline(
           Readable.from(generateUserInfo()), // this source yields 'chunks' of 10 `UserInfo` as `UserInfo[]`s
+          new AttendanceAugmentingTransform(),
+          filterTransform,
           new KeycloakAugmentingTransform(), // this transform consumes 'UserInfo's, then augments them with Keycloak data and yields the new object
           new MlhCsvTransform(), // this transform consumes the augmented 'UserInfo's, picking out required fields for the MLH data
           createWriteStream(fileDestination), // this sink consumes single strings / Buffers at a time
         )
 
-        await response.download(fileDestination, "major-league-hacking-data-export.csv")
+        await response.download(fileDestination, fileName)
       } finally {
         await rm(tempDir, { recursive: true, force: true })
       }
@@ -51,18 +64,28 @@ class DataExportHandlers {
   @onlyGroups([Group.organisers, Group.admins])
   getHackathonsUk(): Middleware {
     return async (request, response) => {
+      const attendeesOnly = request.query.attendees != null
       const tempDir = await getTempDir()
       try {
-        const fileDestination = pathJoin(tempDir, "hackathons-uk-data-export.csv") // the name of the temporary file doesn't actually matter
+        const fileName = attendeesOnly
+          ? "hackathons-uk-attendees.csv"
+          : "hackathons-uk-applicants.csv"
+        const fileDestination = pathJoin(tempDir, fileName) // the name of the temporary file doesn't actually matter
+
+        const filterTransform = attendeesOnly
+          ? new FilteringTransform<UserInfo & AttendanceAugments>((item) => item.isCheckedIn)
+          : new FilteringTransform(() => true)
 
         await pipeline(
           Readable.from(generateUserInfo()), // this source yields 'chunks' of 10 `UserInfo` as `UserInfo[]`s
+          new AttendanceAugmentingTransform(),
+          filterTransform,
           new KeycloakAugmentingTransform(), // this transform consumes 'UserInfo's, then augments them with Keycloak data and yields the new object
           new HukCsvTransform(), // this transform consumes the augmented 'UserInfo's, picking out required fields for the HUK data
           createWriteStream(fileDestination), // this sink consumes single strings / Buffers at a time
         )
 
-        await response.download(fileDestination, "hackathons-uk-data-export.csv")
+        await response.download(fileDestination, fileName)
       } finally {
         await rm(tempDir, { recursive: true, force: true })
       }
@@ -81,11 +104,11 @@ class DataExportHandlers {
         const archiveDir = pathJoin(tempDir, "durhack-cvs")
         await mkdir(archiveDir)
 
-        await pipeline([
+        await pipeline(
           Readable.from(generateUserCv()),
           new KeycloakAugmentingTransform(),
           new CvExportingWritable(archiveDir),
-        ])
+        )
 
         const archivePath = pathJoin(tempDir, "durhack-cvs.zip")
         const zipProcess = exec(
---
+++ server/src/routes/applications/data-export/filtering-transform.ts
@@ -0,0 +1,27 @@
+import stream from "node:stream";
+
+import { isString } from "@/lib/type-guards";
+
+export class FilteringTransform<Item> extends stream.Transform {
+  filterPredicate: (item: Item) => boolean
+
+  constructor(filterPredicate: (item: Item) => boolean) {
+    super({
+      writableObjectMode: true, // the stream expects to receive objects, not a string/binary data
+      readableObjectMode: true, // the stream expects its _transform implementation to push an object (array)
+    })
+    this.filterPredicate = filterPredicate
+  }
+
+  _transform(chunk: Item[], encoding: never, callback: stream.TransformCallback): void {
+    try {
+      const filteredChunk = chunk.filter((item) => this.filterPredicate(item))
+      callback(null, filteredChunk satisfies Item[])
+    }
+    catch (error) {
+      if (error instanceof Error) callback(error)
+      if (isString(error)) callback(new Error(error))
+      callback(new Error(`Something really strange happened. Error object: ${error}`))
+    }
+  }
+}
