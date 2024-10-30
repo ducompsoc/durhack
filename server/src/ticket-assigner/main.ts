365cd39f3dfc80f86eb4edff2397f825cb200f72
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-30 20:41:14 +0000

    fix import

--- server/src/ticket-assigner/main.ts
+++ server/src/ticket-assigner/main.ts
@@ -3,9 +3,9 @@ import { pipeline } from "node:stream/promises"
 
 import { prisma } from "@/database"
 import { KeycloakAugmentingTransform } from "@/lib/keycloak-augmenting-transform"
+import { MailgunMailer } from "@/lib/mailer"
 
 import { AttendeeCheckingTransform } from "./attendee-checking-transform"
-import { MailgunMailer } from "./mailer"
 import { TicketAssigningWritable } from "./ticket-assigning-writable"
 import { generateUserInfoByTicketAssignmentOrder } from "./ticket-order-user-info-async-generator"
 
