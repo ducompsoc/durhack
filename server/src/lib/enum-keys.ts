d460acf2f476330e4d392f00909ef65a7ee9e5ed
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-31 16:47:17 +0000

    add `enumKeys` / `enumValues`

---
+++ server/src/lib/enum-keys.ts
@@ -0,0 +1,8 @@
+// https://petermorlion.com/iterating-a-typescript-enum/
+export function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+    return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
+}
+
+export function enumValues<O extends object, K extends keyof O = keyof O>(obj: O): O[K][] {
+    return enumKeys(obj).map((key) => obj[key]) as O[K][]
+}
