f1312d866402aba89de1f87cf425bd4e7e0a6161
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-05 06:21:12 +0000

    feat: add `allow_http_methods` to 'method not allowed' response

--- server/src/middleware/method-not-allowed.ts
+++ server/src/middleware/method-not-allowed.ts
@@ -1,18 +1,27 @@
 import type { NextFunction } from "@otterhttp/app"
 
 import type { Middleware, Request, Response } from "@/types"
+import { ClientError, HttpStatus } from "@otterhttp/errors"
 
 type HttpVerb = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"
 
 export function methodNotAllowed(allowedMethods: Iterable<HttpVerb>): Middleware {
-  const allowHeaderValue = Array.from(allowedMethods).join(", ")
+  const allowedMethodArray = Array.from(allowedMethods)
+  const allowHeaderValue = allowedMethodArray.join(", ")
   const allowedMethodSet: Set<string> = new Set(allowedMethods)
 
   return (req: Request, res: Response, next: NextFunction): void => {
     if (!req.method || !allowedMethodSet.has(req.method)) {
       res.setHeader("Allow", allowHeaderValue)
-      res.sendStatus(405)
-      return
+      throw new ClientError(`The request method HTTP ${req.method} is not permitted for this resource`, {
+        statusCode: HttpStatus.MethodNotAllowed,
+        code: "ERR_HTTP_METHOD_NOT_ALLOWED",
+        exposeMessage: true,
+        expected: true,
+        extra: {
+          allow_http_methods: allowedMethodArray,
+        },
+      })
     }
 
     next()
