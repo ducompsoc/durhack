9945bf89b939dd89e6d81f5541e903a11b365435
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-14 00:06:45 +0100

    do some more mucking around until it looks a bit better

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -7,10 +7,10 @@ import { RegisterInterestForm } from "./register-interest-form"
 export default function HomePage() {
   return (
     <>
-      <main className="dark leading-8 grid grid-cols-1 text-center justify-center 2xl:grid-cols-2 2xl:text-left">
+      <main className="max-w-[20rem] items-center dark leading-8 flex flex-col text-center justify-center lg:max-w-full 2xl:grid 2xl:grid-cols-2 2xl:text-left">
         <section className="text-white pb-3 border-b border-white 2xl:pb-0 2xl:pr-5 2xl:border-b-0 2xl:border-r">
           <div className="flex flex-col h-full justify-center">
-            <h1 className="text-nowrap uppercase text-6xl 2xl:text-8xl">
+            <h1 className="text-nowrap uppercase text-5xl lg:text-7xl 2xl:text-8xl">
               <span className={cn(audiowide.className)}>DurHack{" "}</span>
               <span
                 className={cn(audiowide.className, "text-transparent")}
@@ -23,13 +23,13 @@ export default function HomePage() {
         </section>
         <section className="text-white py-3 border-b border-white 2xl:pt-0 2xl:pb-0 2xl:pl-5 2xl:border-b-0">
           <div className="flex flex-col h-full justify-center text-center 2xl:text-right">
-            <h2 className={cn(spaceGrotesk.className, "date text-4xl 2xl:text-[4rem]")}>
+            <h2 className={cn(spaceGrotesk.className, "date text-3xl lg:text-5xl 2xl:text-[4rem]")}>
               1<sup>st</sup>-2<sup>nd</sup> November 2025
             </h2>
           </div>
         </section>
         <section className="py-3 text-center 2xl:text-xl 2xl:col-span-2">
-          <p>Celebrating 10 years of DurHack, the UK&apos;s Leading Student Hackathon</p>
+          Celebrating 10 years of DurHack, the UK&apos;s Leading Student Hackathon
         </section>
         <section className="pt-3 2xl:col-span-2">
           <RegisterInterestForm className="grid grid-cols-1 2xl:grid-cols-4 2xl:gap-3" />
