ba4b85193a955782f0063d24cd32d749443124c9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-07-01 22:19:04 +0100

    add MLH banner to homepage closes #218

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -1,16 +1,21 @@
 import type * as React from "react"
 import "@/styles/home.css"
 
+import { MLHBanner } from "@/components/mlh-banner"
+
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>): React.ReactNode {
   return (
-    <div className="bg-homepage-gradient min-h-[100vh] flex-1 flex flex-col content-center items-center justify-center">
-      <div className="w-full min-h-[110px] 2xl:hidden" />
-      {children}
-      <div className="w-full min-h-[110px] 2xl:hidden" />
-    </div>
+    <>
+      <MLHBanner variant="white" season={2026} region="eu" />
+      <div className="bg-homepage-gradient min-h-[100vh] flex-1 flex flex-col content-center items-center justify-center">
+        <div className="w-full min-h-[110px] 2xl:hidden" />
+        {children}
+        <div className="w-full min-h-[110px] 2xl:hidden" />
+      </div>
+    </>
   )
 }
