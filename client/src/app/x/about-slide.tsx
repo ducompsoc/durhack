019e9323e95ff2ec8c1e8cb2014a30b8aee6781f
ItzTJSV2 <ItzTJSV2@gmail.com>
2025-07-07 21:33:53 +0700

    Fixing scroll and padding

--- client/src/app/x/about-slide.tsx
+++ client/src/app/x/about-slide.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils"
 
 export function AboutSlide({ className, ...props }: React.HTMLAttributes<HTMLDivElement>): React.ReactNode {
   return (
-    <article className={cn("absolute size-full flex items-center justify-center", className)} {...props}>
-      <div className="max-w-160 mx-16 flex flex-col space-y-1.5 items-center justify-center">
+    <article className={cn("absolute overflow-y-auto flex justify-center px-8", className)} {...props}>
+      <div className="max-w-[40rem] w-full flex flex-col items-center space-y-1.5 py-16 my-auto">
         <h1 className={cn(electrolize.className, "text-5xl pb-5")}>DurHack X</h1>
         <h2 className={cn(spaceGrotesk.className, "text-3xl")}>600 Attendees</h2>
         <h2 className={cn(spaceGrotesk.className, "text-3xl")}>150 Projects</h2>
