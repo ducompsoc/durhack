1e646d81c2d022349be1123f8e516c86a96952c9
Emre Ozden <tlcj35@durham.ac.uk>
2025-07-11 15:26:55 +0300

    Added DSU Privacy policy and photo/media consent to AnonymousCsvTransform

--- server/src/routes/applications/data-export/anonymous-csv-transform.ts
+++ server/src/routes/applications/data-export/anonymous-csv-transform.ts
@@ -1,21 +1,24 @@
-import {PickAttributesToCsvTransform} from "@/routes/applications/data-export/pick-attributes-to-csv-transform";
-import {UserInfo} from "@/database";
-import { AgeAugment } from "./user-age-augmenting-transform";
-import {KeycloakAugments} from "@/lib/keycloak-augmenting-transform";
-import {ConsentAugments} from "./consent-augmenting-transform";
-import {AttendanceAugments} from "./attendance-augmenting-transform";
+import type { UserInfo } from "@/database"
+import { PickAttributesToCsvTransform } from "@/routes/applications/data-export/pick-attributes-to-csv-transform"
+import type { AttendanceAugments } from "./attendance-augmenting-transform"
+import type { ConsentAugments } from "./consent-augmenting-transform"
+import type { AgeAugment } from "./user-age-augmenting-transform"
 
-export class AnonymousCsvTransform extends PickAttributesToCsvTransform<UserInfo & AgeAugment & AttendanceAugments> {
-  constructor(){
+export class AnonymousCsvTransform extends PickAttributesToCsvTransform<
+  UserInfo & AgeAugment & AttendanceAugments & ConsentAugments<"media" | "dsuPrivacy">
+> {
+  constructor() {
     super({
       attributes: [
-        {name: "countryOfResidence", label: "country_of_residence"},
-        {name: "university", label: "university"},
-        {name: "levelOfStudy", label: "level_of_study"},
-        {name: "ageGroup", label: "age_group"},
-        {name: "hackathonExperience", label: "hackathon_experience"},
-        {name: "tShirtSize", label: "t_shirt_size"},
-        {name: "isCheckedIn", label: "has_attended"}
+        { name: "countryOfResidence", label: "country_of_residence" },
+        { name: "university", label: "university" },
+        { name: "levelOfStudy", label: "level_of_study" },
+        { name: "tShirtSize", label: "t_shirt_size" },
+        { name: "hackathonExperience", label: "hackathon_experience" },
+        { name: "ageGroup", label: "age_group" },
+        { name: "dsuPrivacy", label: "dsu_privacy" },
+        { name: "media", label: "photo_media_consent" },
+        { name: "isCheckedIn", label: "has_attended" },
       ],
     })
   }
