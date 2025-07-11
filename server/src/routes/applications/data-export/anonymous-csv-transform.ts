887097e0c19ad6bc6bae89bfd4c125dc429857ff
Emre Ozden <tlcj35@durham.ac.uk>
2025-07-11 16:31:53 +0300

    Wrote augment to find user CV and last time CV was updated

--- server/src/routes/applications/data-export/anonymous-csv-transform.ts
+++ server/src/routes/applications/data-export/anonymous-csv-transform.ts
@@ -1,11 +1,12 @@
 import type { UserInfo } from "@/database"
 import { PickAttributesToCsvTransform } from "@/routes/applications/data-export/pick-attributes-to-csv-transform"
+import type { UserCvAugment } from "@/routes/applications/data-export/user-cv-augmenting-transform"
 import type { AttendanceAugments } from "./attendance-augmenting-transform"
 import type { ConsentAugments } from "./consent-augmenting-transform"
 import type { AgeAugment } from "./user-age-augmenting-transform"
 
 export class AnonymousCsvTransform extends PickAttributesToCsvTransform<
-  UserInfo & AgeAugment & AttendanceAugments & ConsentAugments<"media" | "dsuPrivacy">
+  UserInfo & AgeAugment & AttendanceAugments & ConsentAugments<"media" | "dsuPrivacy"> & UserCvAugment
 > {
   constructor() {
     super({
@@ -18,6 +19,8 @@ export class AnonymousCsvTransform extends PickAttributesToCsvTransform<
         { name: "ageGroup", label: "age_group" },
         { name: "dsuPrivacy", label: "dsu_privacy" },
         { name: "media", label: "photo_media_consent" },
+        { name: "is_cv_uploaded", label: "has_uploaded_cv" },
+        { name: "cv_update_time", label: "cv_update_time" },
         { name: "isCheckedIn", label: "has_attended" },
       ],
     })
