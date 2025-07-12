bb2fe8c51c9163b3cf69932759d68eb06c9110bd
Emre Ozden <tlcj35@durham.ac.uk>
2025-07-12 23:18:23 +0300

    Added dietary requirements and discipline of study to the csv transformer

--- server/src/routes/applications/data-export/anonymous-csv-transform.ts
+++ server/src/routes/applications/data-export/anonymous-csv-transform.ts
@@ -1,12 +1,18 @@
 import type { UserInfo } from "@/database"
 import { PickAttributesToCsvTransform } from "@/routes/applications/data-export/pick-attributes-to-csv-transform"
 import type { UserCvAugment } from "@/routes/applications/data-export/user-cv-augmenting-transform"
+import type { FlagAugments } from "@/routes/applications/data-export/user-flag-json-augmentor"
 import type { AttendanceAugments } from "./attendance-augmenting-transform"
 import type { ConsentAugments } from "./consent-augmenting-transform"
 import type { AgeAugment } from "./user-age-augmenting-transform"
 
 export class AnonymousCsvTransform extends PickAttributesToCsvTransform<
-  UserInfo & AgeAugment & AttendanceAugments & ConsentAugments<"media" | "dsuPrivacy"> & UserCvAugment
+  UserInfo &
+    AgeAugment &
+    AttendanceAugments &
+    ConsentAugments<"media" | "dsuPrivacy"> &
+    UserCvAugment &
+    FlagAugments<"discipline-of-study" | "dietary-requirement">
 > {
   constructor() {
     super({
@@ -16,6 +22,8 @@ export class AnonymousCsvTransform extends PickAttributesToCsvTransform<
         { name: "levelOfStudy", label: "level_of_study" },
         { name: "tShirtSize", label: "t_shirt_size" },
         { name: "hackathonExperience", label: "hackathon_experience" },
+        { name: "dietary-requirement", label: "dietary_requirements" },
+        { name: "discipline-of-study", label: "discipline_of_study" },
         { name: "ageGroup", label: "age_group" },
         { name: "dsuPrivacy", label: "dsu_privacy" },
         { name: "media", label: "photo_media_consent" },
