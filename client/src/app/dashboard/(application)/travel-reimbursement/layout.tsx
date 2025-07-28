9f31dfd42df6dd18c358b668573546c056073687
Anna-Maria <enn.miray21.12@gmail.com>
2025-07-28 14:46:24 +0100

    edits to travel reimbursement form (travel methods - multiselect, travel receipts upload)

--- client/src/app/dashboard/(application)/travel-reimbursement/layout.tsx
+++ client/src/app/dashboard/(application)/travel-reimbursement/layout.tsx
@@ -1,9 +1,9 @@
 import type * as React from "react"
 
-export default function PostTravelReimbursementFormLayout({ children }: { children: React.ReactNode }) {
+export default function TravelReimbursementFormLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <h2 className="text-2xl">Post Travel Reimbursement Form
+      <h2 className="text-2xl">Travel Reimbursement Form
       </h2>
       {children}
     </>
