20c51bd9fcbc0c4836b0a0afc18963ae6efd2ad7
Joseph Garnett <117537560+joe-garnett@users.noreply.github.com>
2025-08-22 16:40:33 +0100

    add template route

---
+++ client/src/app/template/page.tsx
@@ -0,0 +1,70 @@
+export default function Template() {
+  return (
+    <main className="flex flex-col">
+      {/* Hero */}
+      <div className="hero flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Hero</h1>
+      </div>
+
+      {/* Getting There */}
+      <div className="getting-there flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Getting There</h1>
+      </div>
+
+      {/* Info */}
+      <div className="info flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Info</h1>
+      </div>
+
+      {/* Sponsors */}
+      <div className="sponsors flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Sponsors</h1>
+      </div>
+
+      {/* Partners */}
+      <div className="partners flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Partners</h1>
+      </div>
+
+      {/* FAQ */}
+      <div className="faq flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">FAQ</h1>
+      </div>
+
+      {/* History */}
+      <div className="history flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">History</h1>
+      </div>
+
+      {/* Guilds */}
+      <div className="guilds flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Guilds</h1>
+      </div>
+
+      {/* Schedule */}
+      <div className="schedule flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Schedule</h1>
+      </div>
+
+      {/* Past Projects */}
+      <div className="past-projects flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Past Projects</h1>
+      </div>
+
+      {/* Meet the Team */}
+      <div className="meet-team flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Meet the Team</h1>
+      </div>
+
+      {/* Medium */}
+      <div className="medium flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Medium</h1>
+      </div>
+
+      {/* Footer */}
+      <div className="footer flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Footer</h1>
+      </div>
+    </main>
+  );
+}
