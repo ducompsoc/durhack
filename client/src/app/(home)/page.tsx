aaa469556890dcf3629f27930875308cd7e93e72
Joseph Garnett <117537560+joe-garnett@users.noreply.github.com>
2025-09-01 11:47:57 +0100

    add extra template secitons

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -107,6 +107,7 @@ export default function HomePage() {
               alt="sunflowers"
               className="hidden md:block absolute bottom-[-15%] left-[65%] w-[44.5%] h-auto"
             />
+            {/* EXAMPLE ONLY */}
             <img
               src="/assets/hero/text.svg"
               alt="text"
@@ -122,7 +123,6 @@ export default function HomePage() {
       <div className="getting-there relative flex rounded-t-[9.375vw] overflow-hidden min-h-[899px] md:min-h-[2483px] aspect-[1920/2483]">
         <div className="">
           <div className="">
-        <h1 className="text-white text-5xl font-bold">Getting There</h1>
         <img
           src="/assets/getting-there/city-background.svg"
           alt="City Skyline Background"
@@ -167,54 +167,187 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+
       {/* Info */}
-      <div className="info flex items-start justify-center">
+      <div className="info relative flex overflow-hidden min-h-[778px] md:min-h-[1928px] aspect-[1920/1928]">
         <h1 className="text-white text-5xl font-bold">Info</h1>
+        <img
+          src="/assets/info/hexagons-right.svg"
+          alt="Hexagons showing images"
+          className="absolute bottom-[69%] left-[58.3%] w-[32.9%] h-auto"
+        />
+        <img
+          src="/assets/info/hexagons-left.svg"
+          alt="Hexagons showing images"
+          className="absolute bottom-[19.8%] left-[8.3%] w-[36.1%] h-auto"
+        />
+        <img
+          src="/assets/info/zepplins.svg"
+          alt="Proppoler Zepplins"
+          className="absolute bottom-[2%] left-[64.6%] w-[22.7%] h-auto"
+        />
+        {/* EXAMPLE ONLY */}
+        <img
+          src="/assets/info/info-numbers.svg"
+          alt="Impressive nubmers"
+          className="absolute bottom-[52%] left-[21.7%] w-[63.125%] h-auto"
+        />
       </div>
 
-      {/* Sponsors */}
-      <div className="sponsors flex items-start justify-center">
+      {/* Sponsors - EXAMPLE ONLY*/}
+      <div className="sponsors relative flex overflow-hidden min-h-[624px] md:min-h-[1355px] aspect-[1920/1355]">
         <h1 className="text-white text-5xl font-bold">Sponsors</h1>
+        <img
+          src="/assets/sponsors-assets/sponsors-example.svg"
+          alt="Sponsors"
+          className="absolute bottom-[19.3%] left-[21.2%] w-[57.8%] h-auto"
+        />
+        <img
+          src="/assets/sponsors-assets/zepplin.svg"
+          alt="Zepplin"
+          className="absolute bottom-[0%] left-[-2.7%] w-[23.7%] h-auto"
+        />
       </div>
 
       {/* Partners */}
-      <div className="partners flex items-start justify-center">
+      <div className="partners relative flex overflow-hidden min-h-[657px] md:min-h-[879px] aspect-[1920/879]">
         <h1 className="text-white text-5xl font-bold">Partners</h1>
+        <img
+          src="/assets/partners/partners-example.svg"
+          alt="Partners"
+          className="absolute bottom-[38%] left-[19.5%] w-[61.5%] h-auto"
+        />
+      
       </div>
 
       {/* FAQ */}
-      <div className="faq flex items-start justify-center">
+      <div className="faq relative flex overflow-hidden min-h-[570px] md:min-h-[1063px] aspect-[1920/1063]">
         <h1 className="text-white text-5xl font-bold">FAQ</h1>
+        <img
+          src="/assets/faq/text-example.svg"
+          alt="FAQ"
+          className="absolute bottom-[2%] left-[21%] w-[67.7%] h-auto"
+        />
       </div>
 
       {/* History */}
-      <div className="history flex items-start justify-center">
+      <div className="history relative flex overflow-hidden min-h-[1039px] md:min-h-[2679px] aspect-[1920/2679]">
         <h1 className="text-white text-5xl font-bold">History</h1>
+        <img
+          src="/assets/history/left-branch.svg"
+          alt="Branches"
+          className="absolute bottom-[70.9%] left-[-2.7%] w-[36.5%] h-auto"
+        />
+         <img
+          src="/assets/history/right-branch.svg"
+          alt="Branches"
+          className="absolute bottom-[65.4%] left-[72.9%] w-[30.2%] h-auto"
+        />
+        <img
+          src="/assets/history/hill.svg"
+          alt="Hill"
+          className="absolute bottom-[11.5%] left-[-0.5%] w-[103.75%] max-w-none h-auto z-1"
+        />
+        <img
+          src="/assets/history/grass.svg"
+          alt="Branches"
+          className="absolute bottom-[50%] left-[-7%] w-[112%] max-w-none h-auto z-2"
+        />
+        <img
+          src="/assets/history/bushes.svg"
+          alt="Bushes"
+          className="absolute bottom-[6.6%] left-[-7%] w-[112%] max-w-none h-auto z-2"
+        />
+        <img
+          src="/assets/history/tree1.svg"
+          alt="Tree"
+          className="absolute bottom-[52%] left-[14.1%] w-[19.6%] h-auto"
+        />
+        <img
+          src="/assets/history/tree2.svg"
+          alt="Tree"
+          className="absolute bottom-[54.2%] left-[16.7%] w-[38.75%] h-auto z-1"
+        />
+        <img
+          src="/assets/history/tree3.svg"
+          alt="Tree"
+          className="absolute bottom-[51.8%] left-[35.8%] w-[29.1%] h-auto z-3"
+        />
+         <img
+          src="/assets/history/tree4.svg"
+          alt="Tree"
+          className="absolute bottom-[53.6%] left-[54.375%] w-[23.125%] h-auto z-1"
+        />
+        <img
+          src="/assets/history/tree5.svg"
+          alt="Tree"
+          className="absolute bottom-[52.4%] left-[63.6%] w-[21.8%] h-auto"
+        />
       </div>
 
       {/* Guilds */}
-      <div className="guilds flex items-start justify-center">
+      <div className="guilds relative flex overflow-hidden min-h-[877px] md:min-h-[1055px] aspect-[1920/1055]">
         <h1 className="text-white text-5xl font-bold">Guilds</h1>
+        <img
+          src="/assets/guilds/guilds-example.svg"
+          alt="Guild logos"
+          className="absolute bottom-[23.5%] left-[18.75%] w-[62.3%] h-auto"
+        />
       </div>
 
       {/* Schedule */}
-      <div className="schedule flex items-start justify-center">
+      <div className="schedule relative flex overflow-hidden min-h-[947px] md:min-h-[1356px] aspect-[1920/1356]">
         <h1 className="text-white text-5xl font-bold">Schedule</h1>
+        <img
+          src="/assets/schedule/schedule-example.svg"
+          alt="Schedule"
+          className="absolute bottom-[22.2%] left-[14.1%] w-[73.6%] h-auto"
+        />
       </div>
 
       {/* Past Projects */}
-      <div className="past-projects flex items-start justify-center">
+      <div className="past-projects relative flex overflow-hidden min-h-[523px] md:min-h-[1321px] aspect-[1920/1321]">
         <h1 className="text-white text-5xl font-bold">Past Projects</h1>
+        <img
+          src="/assets/past-projects/past-projects-example.svg"
+          alt="Schedule"
+          className="absolute bottom-[18.4%] left-[9.5%] w-[79.2%] h-auto"
+        />
       </div>
 
       {/* Meet the Team */}
-      <div className="meet-team flex items-start justify-center">
+      <div className="meet-team relative flex overflow-hidden min-h-[442px] md:min-h-[1094px] aspect-[1920/1094]">
         <h1 className="text-white text-5xl font-bold">Meet the Team</h1>
+        <img
+          src="/assets/meet-the-team/team-example.svg"
+          alt="Team"
+          className="absolute bottom-[69.2%] left-[-77.6%] w-[243%] max-w-none h-auto"
+        />
+        <img
+          src="/assets/meet-the-team/descriptor-left-example.svg"
+          alt="Descriptor"
+          className="absolute bottom-[44.1%] left-[12.2%] w-[30.2%] h-auto"
+        />
+        <img
+          src="/assets/meet-the-team/descriptor-right-example.svg"
+          alt="Descriptor"
+          className="absolute bottom-[44.1%] left-[56.7%] w-[30.2%] h-auto"
+        />
       </div>
 
       {/* Medium */}
-      <div className="medium flex items-start justify-center">
+      <div className="medium relative flex overflow-hidden min-h-[620px] md:min-h-[1284px] aspect-[1920/1284]">
         <h1 className="text-white text-5xl font-bold">Medium</h1>
+        <img
+          src="/assets/medium/medium-example.svg"
+          alt="Medium"
+          className="absolute bottom-[36.6%] left-[13.9%] w-[79.8%] h-auto"
+        />
+        <img
+          src="/assets/medium/cloud.svg"
+          alt="Cloud"
+          className="absolute top-[18.9%] left-[-11.5%] w-[139.3%] max-w-none h-auto rotate-[18.11deg]"
+        />
       </div>
 
       {/* Footer */}
