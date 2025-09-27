f0df960c21c733ee58a76f9c8d8ace4c76f88da2
Emre Ozden <tlcj35@durham.ac.uk>
2025-09-28 02:04:29 +0300

    Added rest of hero assets - need positioning and responsiveness :(

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -4,12 +4,12 @@ import { cn } from "@/lib/utils"
 
 export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="hero relative flex items-start justify-center h-screen">
-      <div className="flex-row items-center">
+    <div className="hero relative flex items-start justify-center h-screen w-full">
+      <div className="flex-row items-center z-50">
         <h1
           className={cn(
             audiowide.className,
-            "text-white uppercase mt-34 mb-12 lg:text-9xl md:text-7xl text-5xl text-shadow-lg/25 z-50 text-center text-shadow-black font-normal",
+            "text-white uppercase mt-34 mb-12 lg:text-9xl md:text-7xl text-5xl text-shadow-lg/25 text-center text-shadow-black font-normal",
           )}
         >
           DurHack X
@@ -17,7 +17,7 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
         <h2
           className={cn(
             spaceGrotesk.className,
-            "text-[#238CBA] lg:text-7xl md:text-5xl text-3xl font-extralight text-center z-50",
+            "text-[#238CBA] lg:text-7xl md:text-5xl text-3xl font-extralight text-center",
           )}
         >
           1st-2nd November
@@ -27,7 +27,7 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
             href="/dashboard"
             className={cn(
               spaceGrotesk.className,
-              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 lg:text-3xl text-xl z-50 font-medium hover:bg-[#238CBA] transition-colors duration-300",
+              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 lg:text-3xl text-xl font-medium hover:bg-[#238CBA] transition-colors duration-300",
             )}
           >
             Sign Up Now
@@ -45,6 +45,21 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
         src="/assets/hero/sun-aura.svg"
         alt="sun aura"
       />
+      <img className={cn("absolute left-0 bottom-0 lg:w-[55vw] w-[75vw] h-auto z-20")} src="/assets/hero/left-cloud.svg" alt="left cloud"/>
+      <img className={cn("absolute left-0 bottom-0 h-auto z-20")} src="/assets/hero/left-cloud-shadow.svg" alt="left cloud shadow"/>
+      <img className={cn("absolute right-0 bottom-0 lg:w-[53vw] w-[75vw] h-auto z-20")} src="/assets/hero/right-cloud.svg" alt="right cloud"/>
+      <img className={cn("absolute right-0 bottom-0 lg:w-[45vw] h-auto z-20")} src="/assets/hero/right-cloud-shadow.svg" alt="right cloud shadow"/>
+      <img className={cn("absolute bottom-0 left-0 w-full z-30")} src="/assets/hero/ground1.svg" alt="ground"/>
+      <img className={cn("absolute bottom-0 right-0 w-full z-30")} src="/assets/hero/ground2.svg" alt="ground"/>
+      <img className={cn("absolute bottom-0 left-0 w-full z-30")} src="/assets/hero/ground3.svg" alt="ground"/>
+      <img className={cn("absolute left-0 bottom-2/12 h-auto z-40")} src="/assets/hero/cathedral.svg" alt="cathedral"/>
+      <img className={cn("absolute left-0 bottom-2/12 h-auto z-40")} src="/assets/hero/left-bush.svg" alt="left bush"/>
+      <img className={cn("absolute right-0 bottom-[-9vh] h-auto z-40")} src="/assets/hero/right-bush.svg" alt="left bush"/>
+      <img className={cn("absolute bottom-2/6 right-1/12 h-auto z-40")} src="/assets/hero/biosphere1.svg" alt="biosphere 1"/>
+      <img className={cn("absolute bottom-2/6 right-3/12 h-auto z-40")} src="/assets/hero/biosphere2.svg" alt="biosphere 2"/>
+      <img className={cn("absolute bottom-6/12 right-6/24 h-[20rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
+      <img className={cn("absolute bottom-6/12 right-2/24 h-[15rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
+      <img className={cn("absolute bottom-6/12 right-7/24 h-[15rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
     </div>
   )
 }
