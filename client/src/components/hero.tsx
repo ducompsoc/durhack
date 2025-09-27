6fd6cbabe89e87e8b287b209f2037c48f22df346
Emre Ozden <tlcj35@durham.ac.uk>
2025-09-28 00:15:53 +0300

    Added sun and sun aura

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -4,12 +4,12 @@ import { cn } from "@/lib/utils"
 
 export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="hero relative flex items-start justify-center">
+    <div className="hero relative flex items-start justify-center h-screen">
       <div className="flex-row items-center">
         <h1
           className={cn(
             audiowide.className,
-            "text-white uppercase mt-44 mb-12 lg:text-9xl md:text-7xl text-5xl text-shadow-lg/25 text-center text-shadow-black font-normal",
+            "text-white uppercase mt-34 mb-12 lg:text-9xl md:text-7xl text-5xl text-shadow-lg/25 z-50 text-center text-shadow-black font-normal",
           )}
         >
           DurHack X
@@ -17,7 +17,7 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
         <h2
           className={cn(
             spaceGrotesk.className,
-            "text-[#238CBA] lg:text-7xl md:text-5xl text-3xl font-extralight text-center",
+            "text-[#238CBA] lg:text-7xl md:text-5xl text-3xl font-extralight text-center z-50",
           )}
         >
           1st-2nd November
@@ -27,13 +27,24 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
             href="/dashboard"
             className={cn(
               spaceGrotesk.className,
-              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 lg:text-3xl text-xl font-medium hover:bg-[#238CBA] transition-colors duration-300",
+              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 lg:text-3xl text-xl z-50 font-medium hover:bg-[#238CBA] transition-colors duration-300",
             )}
           >
             Sign Up Now
           </a>
         </div>
       </div>
+
+      <img
+        className={cn("absolute lg:bottom-2/6 lg:w-auto w-50 bottom-13/24 h-auto z-10")}
+        src="/assets/hero/sun.svg"
+        alt="sun"
+      />
+      <img
+        className={cn("absolute lg:top-1/6 lg:w-auto top-3/12 w-[200%] z-5")}
+        src="/assets/hero/sun-aura.svg"
+        alt="sun aura"
+      />
     </div>
   )
 }
