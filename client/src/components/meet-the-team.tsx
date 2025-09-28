88c2d171fbdf4405c57f0707c93d4d438f322d4a
Emre Ozden <tlcj35@durham.ac.uk>
2025-09-29 02:49:50 +0300

    added avatar pointer img on mouse hover

--- client/src/components/meet-the-team.tsx
+++ client/src/components/meet-the-team.tsx
@@ -7,34 +7,32 @@ import {teammates} from "@/config/teammates";
 
 function TeamCarousel({...props}: React.HTMLAttributes<HTMLDivElement>) {
   const [scrolling, setScrolling] = React.useState(true)
-  function pauseScroll() {
+  function pauseScroll(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
     setScrolling(false)
+    e.currentTarget.nextElementSibling?.classList.remove("hidden")
   }
 
-  function startScroll() {
+  function startScroll(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
     setScrolling(true)
+    e.currentTarget.nextElementSibling?.classList.add("hidden")
   }
 
   return (
     <div className={cn(props.className, "w-full inline-flex flex-nowrap overflow-x-hidden")}>
       <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")}>
         {teammates.map((teammate, index) => (
-          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
+          <li key={index} className={cn("relative")}>
+            <img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/>
+            <img className={cn("absolute bottom-0 right-0 hidden")} src="/assets/meet-the-team/teammate-pointer.svg" alt="teammate-pointer.svg"/>
+          </li>
         ))}
       </ul>
       <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")} aria-hidden="true">
         {teammates.map((teammate, index) => (
-          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
-        ))}
-      </ul>
-      <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")} aria-hidden="true">
-        {teammates.map((teammate, index) => (
-          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
-        ))}
-      </ul>
-      <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")} aria-hidden="true">
-        {teammates.map((teammate, index) => (
-          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
+          <li key={index} className={cn("relative")}>
+            <img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/>
+            <img className={cn("absolute bottom-0 right-0 hidden")} src="/assets/meet-the-team/teammate-pointer.svg" alt="teammate-pointer.svg"/>
+          </li>
         ))}
       </ul>
     </div>
