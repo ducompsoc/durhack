34921bf3c0659a95cf86ba4b59dbd53ce0c15630
Emre Ozden <tlcj35@durham.ac.uk>
2025-09-29 01:31:08 +0300

    Added working infinite carousel that pauses when someone's avatar is hovered over

--- client/src/components/meet-the-team.tsx
+++ client/src/components/meet-the-team.tsx
@@ -1,10 +1,48 @@
-import type React from "react"
+"use client"
+
+import React from "react"
 import { SectionHeader } from "./section-header"
+import {cn} from "@/lib/utils";
+import {teammates} from "@/config/teammates";
+
+function TeamCarousel({...props}: React.HTMLAttributes<HTMLDivElement>) {
+  const [scrolling, setScrolling] = React.useState(true)
+  function pauseScroll() {
+    setScrolling(false)
+  }
+
+  function startScroll() {
+    setScrolling(true)
+  }
+
+  return (
+    <div className={cn(props.className, "w-full inline-flex flex-nowrap overflow-x-hidden")}>
+      <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")}>
+        {teammates.map((teammate, index) => (
+          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
+        ))}
+      </ul>
+      <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")} aria-hidden="true">
+        {teammates.map((teammate, index) => (
+          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
+        ))}
+      </ul>
+      <ul className={cn("flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-[150px] animate-infinite-scroll", !scrolling && "animation-paused")} aria-hidden="true">
+        {teammates.map((teammate, index) => (
+          <li key={index}><img onMouseEnter={pauseScroll} onMouseLeave={startScroll} className={cn("rounded-full")} src={teammate.img_path} alt="teammate image"/></li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export function MeetTheTeam({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="meet-team flex items-start justify-center">
-      <SectionHeader className="text-white">Meet The team</SectionHeader>
+    <div className="meet-team">
+      <div className="flex-row items-start justify-center">
+        <SectionHeader className="text-white">Meet The team</SectionHeader>
+        <TeamCarousel className="mt-10"/>
+      </div>
     </div>
   )
 }
--- client/src/config/teammates.tsx
+++ client/src/config/teammates.tsx
@@ -1,9 +1,34 @@
 export type Teammate = {
   name: string
   team: string
+  role: string
   img_path: string
 }
 
 export const teammates: Teammate[] = [
   // TODO add literally all of our beloved teammates here <3
+  {
+    name: "Joe",
+    team: "Teach",
+    role: "Lead",
+    img_path: "/assets/teammates/joe.png"
+  },
+  {
+    name: "Karl",
+    team: "",
+    role: "",
+    img_path: "/assets/teammates/karl.png"
+  },
+  {
+    name: "Liz",
+    team: "",
+    role: "",
+    img_path: "/assets/teammates/liz.png"
+  },
+  {
+    name: "Oscar",
+    team: "",
+    role: "",
+    img_path: "/assets/teammates/oscar.png"
+  }
 ]
