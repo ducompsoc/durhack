89a6fb359d0dabc0114f13b23c119299c377212f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-09-29 08:23:55 +0100

    something of a hero section, maybe

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -1,15 +1,59 @@
 import type React from "react"
+
 import { audiowide, spaceGrotesk } from "@/lib/google-fonts"
 import { cn } from "@/lib/utils"
+import { HillTopLeftGraphic } from "@/components/graphics/hill-top-left";
+import { HillRightGraphic } from "@/components/graphics/hill-right";
+import { HillBottomLeftGraphic } from "@/components/graphics/hill-bottom-left";
+import { CloudsLeftGraphic } from "@/components/graphics/clouds-left";
+import { CloudsRightGraphic } from "@/components/graphics/clouds-right";
+import Image from "next/image";
 
-export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
+export function Hero({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="hero relative flex items-start justify-center h-screen w-full">
-      <div className="flex-row items-center z-50">
+    <div className={cn("hero h-[calc(40rem+35vw)] sm:h-220 md:h-240 lg:h-260 xl:h-300 2xl:h-260 relative flex items-start justify-center overflow-hidden", className)} {...props}>
+      <div className="absolute h-full hidden 2xl:contents 2xl:w-full top-10">
+        <div className="absolute -translate-1/2 left-1/2 top-200 size-300 rounded-full bg-radial from-[#FFFAE1D4] from-30% to-[#FFE6FA00] to-70% overflow-visible" />
+        <div className="absolute -translate-1/2 left-1/2 top-170 size-80 rounded-full bg-radial from-[#F7F2DB] to-[#FFD700]" />
+
+        <CloudsLeftGraphic className="absolute left-[-5%] top-35 w-[56%]" />
+        <CloudsLeftGraphic stroke="#FF5959" className="absolute left-[-10%] top-60 w-[56%] blur-sm text-[#C1E5FD]" />
+        <CloudsRightGraphic className="absolute right-[-12%] top-5 w-[60%]" />
+        <CloudsRightGraphic stroke="#FF5959" className="absolute right-[-17%] top-30 w-[60%] blur-sm text-[#C1E5FD]" />
+
+        <HillTopLeftGraphic className="absolute left-[-20%] top-120 w-full" />
+        <HillRightGraphic className="absolute right-[-20%] top-140 w-full" />
+        <HillBottomLeftGraphic className="absolute left-[-20%] top-200 w-full" />
+
+        <Image
+          src="/assets/graphics/cathedral.svg" alt="Durham Cathedral" width={724} height={582}
+          className="absolute left-0 top-100"
+        />
+      </div>
+
+      <div className="absolute h-full 2xl:hidden w-full top-10">
+        <div className="absolute -translate-1/2 left-1/2 top-180 size-300 rounded-full bg-radial from-[#FFFAE1D4] from-30% to-[#FFE6FA00] to-70% overflow-visible" />
+        <div className="absolute -translate-1/2 left-1/2 top-130 sm:top-150 md:top-160 lg:top-180 xl:top-200 size-50 lg:size-70 rounded-full bg-radial from-[#F7F2DB] to-[#FFD700]" />
+
+        <CloudsLeftGraphic className="absolute left-[-5%] top-105 w-[60%]" />
+        <CloudsRightGraphic className="absolute right-[-12%] top-105 w-[70%]" />
+        <CloudsLeftGraphic stroke="#FF5959" className="absolute left-[-10%] top-110 w-[56%] blur-sm text-[#C1E5FD]" />
+        <CloudsRightGraphic stroke="#FF5959" className="absolute right-[-17%] top-110 w-[60%] blur-sm text-[#C1E5FD]" />
+
+        <HillRightGraphic className="absolute right-[-40%] top-[calc(30rem+20vw)] w-[150%]" />
+        <HillBottomLeftGraphic className="absolute left-[-30%] top-[calc(35rem+20vw)] w-[150%]" />
+
+        <Image
+          src="/assets/graphics/cathedral.svg" alt="Durham Cathedral" width={724} height={582}
+          className="absolute left-[-15%] md:left-0 top-[calc(40rem+30vw)] -translate-y-full w-2/3 max-w-140"
+        />
+      </div>
+
+      <div className="flex-row items-center z-10">
         <h1
           className={cn(
             audiowide.className,
-            "text-white uppercase mt-34 mb-12 lg:text-9xl md:text-7xl text-5xl text-shadow-lg/25 text-center text-shadow-black font-normal",
+            "text-white uppercase mt-44 mb-12 md:text-7xl text-5xl text-shadow-lg/25 text-center text-shadow-black font-normal",
           )}
         >
           DurHack X
@@ -17,7 +61,7 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
         <h2
           className={cn(
             spaceGrotesk.className,
-            "text-[#238CBA] lg:text-7xl md:text-5xl text-3xl font-extralight text-center",
+            "text-[#238CBA] md:text-5xl text-3xl font-extralight text-center",
           )}
         >
           1st-2nd November
@@ -27,39 +71,13 @@ export function Hero(props: React.HTMLAttributes<HTMLDivElement>) {
             href="/dashboard"
             className={cn(
               spaceGrotesk.className,
-              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 lg:text-3xl text-xl font-medium hover:bg-[#238CBA] transition-colors duration-300",
+              "uppercase bg-[#6DB7D2] rounded-full mt-10 py-3 px-12 text-xl font-medium hover:bg-[#238CBA] transition-colors duration-300",
             )}
           >
             Sign Up Now
           </a>
         </div>
       </div>
-
-      <img
-        className={cn("absolute lg:bottom-2/6 lg:w-auto w-50 bottom-13/24 h-auto z-10")}
-        src="/assets/hero/sun.svg"
-        alt="sun"
-      />
-      <img
-        className={cn("absolute lg:top-1/6 lg:w-auto top-3/12 w-[200%] z-5")}
-        src="/assets/hero/sun-aura.svg"
-        alt="sun aura"
-      />
-      <img className={cn("absolute left-0 bottom-0 lg:w-[55vw] w-[75vw] h-auto z-20")} src="/assets/hero/left-cloud.svg" alt="left cloud"/>
-      <img className={cn("absolute left-0 bottom-0 h-auto z-20")} src="/assets/hero/left-cloud-shadow.svg" alt="left cloud shadow"/>
-      <img className={cn("absolute right-0 bottom-0 lg:w-[53vw] w-[75vw] h-auto z-20")} src="/assets/hero/right-cloud.svg" alt="right cloud"/>
-      <img className={cn("absolute right-0 bottom-0 lg:w-[45vw] h-auto z-20")} src="/assets/hero/right-cloud-shadow.svg" alt="right cloud shadow"/>
-      <img className={cn("absolute bottom-0 left-0 w-full z-30")} src="/assets/hero/ground1.svg" alt="ground"/>
-      <img className={cn("absolute bottom-0 right-0 w-full z-30")} src="/assets/hero/ground2.svg" alt="ground"/>
-      <img className={cn("absolute bottom-0 left-0 w-full z-30")} src="/assets/hero/ground3.svg" alt="ground"/>
-      <img className={cn("absolute left-0 bottom-2/12 h-auto z-40")} src="/assets/hero/cathedral.svg" alt="cathedral"/>
-      <img className={cn("absolute left-0 bottom-2/12 h-auto z-40")} src="/assets/hero/left-bush.svg" alt="left bush"/>
-      <img className={cn("absolute right-0 bottom-[-9vh] h-auto z-40")} src="/assets/hero/right-bush.svg" alt="left bush"/>
-      <img className={cn("absolute bottom-2/6 right-1/12 h-auto z-40")} src="/assets/hero/biosphere1.svg" alt="biosphere 1"/>
-      <img className={cn("absolute bottom-2/6 right-3/12 h-auto z-40")} src="/assets/hero/biosphere2.svg" alt="biosphere 2"/>
-      <img className={cn("absolute bottom-6/12 right-6/24 h-[20rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
-      <img className={cn("absolute bottom-6/12 right-2/24 h-[15rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
-      <img className={cn("absolute bottom-6/12 right-7/24 h-[15rem] z-40")} src="/assets/hero/windmill.svg" alt="windmill"/>
     </div>
   )
 }
