c51a925ecd53db4bbdaa2cd99667291180e12b0b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-06 19:12:07 +0100

    add hours and minutes to event timing info object

--- server/src/lib/format-event-timings.ts
+++ server/src/lib/format-event-timings.ts
@@ -8,25 +8,35 @@ export type DurHackEventTimingInfo = {
   startDate: number
   startDateOrdinalSuffix: string
   startDay: string
+  startHour: number
+  startMinute: number
 
   endMonth: string
   endDate: number
   endDateOrdinalSuffix: string
   endDay: string
+  endHour: number
+  endMinute: number
 }
 
 export function getEventTimingInfo(): DurHackEventTimingInfo {
   const currentEventYear = durhackConfig.currentEventStart.getFullYear()
 
+  new Date().getHours()
+
   const startMonth = durhackConfig.currentEventStart.toLocaleString("en-GB", { month: "long" })
   const startDate = durhackConfig.currentEventStart.getDate()
   const startDateOrdinalSuffix = getOrdinalSuffix(startDate)
   const startDay = durhackConfig.currentEventStart.toLocaleString("en-GB", { weekday: "long" })
+  const startHour = durhackConfig.currentEventStart.getHours()
+  const startMinute = durhackConfig.currentEventStart.getMinutes()
 
   const endMonth = durhackConfig.currentEventEnd.toLocaleString("en-GB", { month: "long" })
   const endDate = durhackConfig.currentEventEnd.getDate()
   const endDateOrdinalSuffix = getOrdinalSuffix(endDate)
   const endDay = durhackConfig.currentEventEnd.toLocaleString("en-GB", { weekday: "long" })
+  const endHour = durhackConfig.currentEventEnd.getHours()
+  const endMinute = durhackConfig.currentEventEnd.getMinutes()
 
   return {
     currentEventYear,
@@ -34,9 +44,13 @@ export function getEventTimingInfo(): DurHackEventTimingInfo {
     startDate,
     startDateOrdinalSuffix,
     startDay,
+    startHour,
+    startMinute,
     endMonth,
     endDate,
     endDateOrdinalSuffix,
     endDay,
+    endHour,
+    endMinute,
   }
 }
