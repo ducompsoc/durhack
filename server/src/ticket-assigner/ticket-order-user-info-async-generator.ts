a3e3b928e5619179a67519df3353cc1d1209d551
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 14:50:48 +0100

    adjust ticket assigner to also use unified `generateUserInfo`

--- server/src/ticket-assigner/ticket-order-user-info-async-generator.ts
+++ server/src/ticket-assigner/ticket-order-user-info-async-generator.ts
@@ -1,20 +1,9 @@
-import { prisma, type UserInfo } from "@/database"
+import type { UserInfo } from "@/database"
+import { generateUserInfo } from "@/lib/user-info-async-generator"
 
 export async function* generateUserInfoByTicketAssignmentOrder(): AsyncGenerator<UserInfo[], undefined> {
-  let cursor: string | undefined
-  do {
-    const results = await prisma.userInfo.findMany({
-      take: 10,
-      skip: cursor == null ? 0 : 1,
-      cursor: cursor == null ? undefined : { userId: cursor },
-      where: {
-        OR: [{ applicationStatus: { in: ["submitted", "waitingList"] } }, { userId: cursor }],
-      },
-      orderBy: [{ applicationSubmittedAt: "asc" }, { userId: "asc" }],
-    })
-
-    cursor = results[9]?.userId
-    console.log(`query returned ${results.length}, cursor is ${cursor}`)
-    yield results
-  } while (cursor != null)
+  yield* generateUserInfo({
+    where: { applicationStatus: { in: ["submitted", "waitingList"] } },
+    orderBy: [{ applicationSubmittedAt: "asc" }],
+  })
 }
