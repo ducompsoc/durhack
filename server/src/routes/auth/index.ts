a8a49c28863c6698ee38c701bdbecaedf7792e49
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 02:45:55 +0100

    add a some static navigational URLs to API responses

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,10 +1,22 @@
 import { App } from "@otterhttp/app"
 
+import { origin } from "@/config"
+import { methodNotAllowed } from "@/middleware/method-not-allowed"
+import { serveStaticJson } from "@/middleware/serve-static-json"
 import type { Request, Response } from "@/types"
 
 import { keycloakApp } from "./keycloak"
 
 const authApp = new App<Request, Response>()
+
+authApp
+  .route("/")
+  .all(methodNotAllowed(["GET"]))
+  .get(
+    serveStaticJson({
+      keycloak_auth_url: new URL("/auth/keycloak", origin).href,
+    }),
+  )
 
 authApp.use("/keycloak", keycloakApp)
 
--- server/src/routes/calendar/index.ts
+++ server/src/routes/calendar/index.ts
@@ -1,12 +1,25 @@
 import { App } from "@otterhttp/app"
-import { durhackConfig } from "@/config"
+
+import { durhackConfig, origin } from "@/config"
 import { methodNotAllowed } from "@/middleware/method-not-allowed"
+import { serveStaticJson } from "@/middleware/serve-static-json"
 import type { Request, Response } from "@/types"
+
 import { getCalendarEvent } from "./calendar-event"
 
 export const calendarApp = new App<Request, Response>()
 
 const currentEventYear = durhackConfig.currentEventStart.getFullYear()
+const eventInvitePath = `/durhack-${currentEventYear}`
+
+calendarApp
+  .route("/")
+  .all(methodNotAllowed(["GET"]))
+  .get(
+    serveStaticJson({
+      event_invite_url: new URL(`/calendar${eventInvitePath}`, origin).href,
+    }),
+  )
 
 calendarApp
   .route(`/durhack-${currentEventYear}`)
