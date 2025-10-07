020d0667cce96da170df1ef902015ad2a8297e5b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 01:01:34 +0100

    add `isObject` to client project

---
+++ client/src/types/extra-utility-types.ts
@@ -0,0 +1,43 @@
+/**
+ * Include null and undefined in T
+ */
+export type Nullable<T> = T | null | undefined
+
+/**
+ * Make all properties in T nullable
+ */
+export type AllNullable<T> = {
+  [P in keyof T]: Nullable<T>
+}
+
+/**
+ * Make all properties in T non-nullable
+ */
+export type AllNonNullable<T> = {
+  [P in keyof T]: NonNullable<T[P]>
+}
+
+/**
+ * Make all properties in T nullable and optional
+ */
+export type Incomplete<T> = {
+  [P in keyof T]?: Nullable<T[P]>
+}
+
+/**
+ * Make all properties in T non-nullable and required
+ */
+export type Complete<T> = {
+  [P in keyof T]-?: NonNullable<T[P]>
+}
+
+/**
+ * An object whose set of properties and their types is unknown
+ */
+// biome-ignore lint/suspicious/noExplicitAny: to allow the key type to be any (unknown) type
+export type UnknownObject = { [P in keyof any]?: unknown }
+
+/**
+ * Construct a type with unknown properties of type T
+ */
+export type StringRecord<T> = UnknownObject & { [P in string]?: Nullable<T> }
