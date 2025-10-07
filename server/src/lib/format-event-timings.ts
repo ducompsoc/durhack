5557fdee7244ea382f13d40c4d3c4b644aef793d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 01:31:17 +0100

    move `getTimeFormattingValues`, `getOrdinalSuffix` to `common`

--- server/src/lib/format-event-timings.ts
+++ server/src/lib/format-event-timings.ts
@@ -1,26 +1,6 @@
+import { getTimeFormattingValues, type TimeFormattingValues } from "@durhack/durhack-common/util/format-date"
+
 import { durhackConfig } from "@/config"
-import { getOrdinalSuffix } from "@/lib/ordinal-suffix"
-
-export type TimeFormattingValues = {
-  month: string
-  date: number
-  dateOrdinalSuffix: string
-  day: string
-  hour: number
-  minute: number
-  time: string
-}
-
-export function getTimeFormattingValues(forWhen: Date): TimeFormattingValues {
-  const month = forWhen.toLocaleString("en-GB", { month: "long" })
-  const date = forWhen.getDate()
-  const dateOrdinalSuffix = getOrdinalSuffix(date)
-  const day = forWhen.toLocaleString("en-GB", { weekday: "long" })
-  const hour = forWhen.getHours()
-  const minute = forWhen.getMinutes()
-  const time = forWhen.toLocaleString("en-GB", { hour: "2-digit", minute: "2-digit" })
-  return { month, date, dateOrdinalSuffix, day, hour, minute, time }
-}
 
 export type DurHackEventTimingInfo = {
   currentEventYear: number
