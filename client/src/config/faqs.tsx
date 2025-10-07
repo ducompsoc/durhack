e95b2a4f706fb1c5bcf1b8af4e8baf581d611fed
“technologeve” <eve.nicole@icloud.com>
2025-10-07 13:28:40 +0100

    Travel reimbursement changes - coach link tbc

--- client/src/config/faqs.tsx
+++ client/src/config/faqs.tsx
@@ -65,21 +65,23 @@ export const faqs = [
     icon_path: "/assets/faq/icon-3.svg",
     answer: (
       <>
-        <p>
-          DurHack will be offering partial-to-full travel reimbursement (depending on your region). Please keep your
-          receipt(s): we will release a form shortly after 5th November for you to put in your details and upload the
-          receipt(s). Reimbursement should come through by 17th December.
-        </p>
-        <p>We have the following regional caps: </p>
-        <p>
-          20 miles (e.g. Newcastle) - £10, 20-100 miles (e.g. Leeds, Lancaster) - £40, 100+ miles (e.g. London, Oxford)
-          - £65{" "}
-        </p>
-        <p>
-          (These should cover most standard train tickets - if you need to travel in an alternative way that may be more
-          expensive, or your train is more than the price above and you need extra support in paying for it, please
-          reach out to us providing more information).
-        </p>
+        <p>Hackers travelling from Manchester, Leeds, Nottingham or Sheffield can obtain reduced-price coaches directly to DurHack at <a href="[link]">[link]</a>. Please note you must have a ticket to DurHack to attend the event.</p>,
+        <p>Hackers from the above cities who do not travel via the DurHack organised coach will not be eligible for travel reimbursement, unless with prior written exception.</p>,
+        <p>For hackers travelling from outside of these cities, we can provide the following reimbursements for reasonable public transport expenses:</p>
+        <ul>
+          <li>&lt;20 miles (e.g. Newcastle) — £10</li>
+          <li>20–100 miles (e.g. Lancaster) — £30</li>
+          <li>100+ miles (e.g. London, Oxford) — £55</li>
+        </ul>
+        <p>If you are travelling in a group by car, or have any other travel queries, please reach out to <a href="mailto:travel@durhack.com">travel@durhack.com</a>.</p>
+
+        <p>All travel reimbursement is conditional on:</p>
+        <ul>
+          <li>Having a DurHack ticket</li>
+          <li>Attending DurHack and being checked in at the event</li>
+          <li>Devpost project submission</li>
+        </ul>
+        <p>Please keep your receipt(s): we will release a form shortly after DurHack for you to put in your details and upload the receipt(s). Reimbursement should come through by the end of December.</p>
       </>
     ),
   },
