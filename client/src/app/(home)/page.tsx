bca16c88d4cdc411292a05309c02393f5592f646
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 01:40:25 +0100

    Reapply "un-hide all the sections"
    This reverts commit bdd3e1f6c4a7d3e8bb0da89a411733ddb4e1acb1.

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -12,46 +12,46 @@ export default function HomePage() {
       <Hero />
 
       {/* Info */}
-      <Info className="hidden" />
+      <Info />
 
       {/* Location */}
-      <div className="location flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">Location</h1>
+      <div className="location flex items-start justify-center ">
+        <h1 className="text-white text-5xl font-bold">Location</h1>
       </div>
 
       {/* Sponsors & Partners */}
-      <Sponsors className="hidden" />
-      <Partners className="hidden" />
+      <Sponsors />
+      <Partners />
 
       {/* FAQ */}
-      <Faqs className="hidden" />
+      <Faqs />
 
       {/* History */}
-      <div className="history flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">History</h1>
+      <div className="history flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">History</h1>
       </div>
 
       {/* Guilds */}
       <Guilds className="hidden" />
 
       {/* Schedule */}
-      <div className="schedule flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">Schedule</h1>
+      <div className="schedule flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Schedule</h1>
       </div>
 
       {/* Past Projects */}
-      <div className="past-projects flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">Past Projects</h1>
+      <div className="past-projects flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Past Projects</h1>
       </div>
 
       {/* Meet the Team */}
-      <div className="meet-team flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">Meet the Team</h1>
+      <div className="meet-team flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Meet the Team</h1>
       </div>
 
       {/* Medium */}
-      <div className="medium flex items-start justify-center hidden">
-        <h1 className="hidden text-white text-5xl font-bold">Medium</h1>
+      <div className="medium flex items-start justify-center">
+        <h1 className="text-white text-5xl font-bold">Medium</h1>
       </div>
 
       {/* Footer - temporarily using the dashboard one */}
