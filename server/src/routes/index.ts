af16ce49a15112548f5f92e38fad9b769ac310c2
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-07 02:26:55 +0100

    add some URLs to the root route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { App } from "@otterhttp/app"
 
+import { origin } from "@/config"
 import { methodNotAllowed } from "@/middleware/method-not-allowed"
+import { serveStaticJson } from "@/middleware/serve-static-json"
 import { applicationApp } from "@/routes/application"
 import { applicationsApp } from "@/routes/applications"
 import { authApp } from "@/routes/auth"
@@ -16,9 +18,18 @@ export const routesApp = new App<Request, Response>()
 routesApp
   .route("/")
   .all(methodNotAllowed(["GET"]))
-  .get((_request, response) => {
-    response.sendStatus(200)
-  })
+  .get(
+    serveStaticJson({
+      applications_url: new URL("/applications", origin).href,
+      auth_url: new URL("/auth", origin).href,
+      calendar_url: new URL("/calendar", origin).href,
+      meta_url: new URL("/meta", origin).href,
+      // profile_url: new URL("/profile", origin).href, // hidden as profile list endpoint doesn't exist
+      register_interest_url: new URL("/register-interest", origin).href,
+      you_url: new URL("/user", origin).href,
+      your_application_url: new URL("/application", origin).href,
+    }),
+  )
 
 routesApp.use("/applications", applicationsApp)
 routesApp.use("/auth", authApp)
