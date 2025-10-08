48f53c74c423e58b0be9d4f7e38d403c59c3a820
Emre <tlcj35@durham.ac.uk>
2025-10-08 23:29:54 +0100

    Made 1 team card for both left and right variants, removed redundant defns

--- client/src/components/meet-the-team.tsx
+++ client/src/components/meet-the-team.tsx
@@ -1,43 +1,21 @@
 "use client"
 
-import React from "react"
-import { LeftTeamCard, RightTeamCard } from "@/components/team-card"
+import type React from "react"
+import { TeamCard } from "@/components/team-card"
 import { type Teammate, teammates } from "@/config/teammates"
 import { cn } from "@/lib/utils"
 import { SectionHeader } from "./section-header"
 
-type HoverState = {
-  teammate: Teammate
-  orientation: "left" | "right"
-  index: number
-}
-
 function TeamCarousel({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  const [scrolling, setScrolling] = React.useState(true)
-  const [hoverState, setHoverState] = React.useState<HoverState | null>(null)
-
-  const [isOpen, setIsOpen] = React.useState(false)
-
-  const TRANSITION_DURATION = 10
-
-  const getCardOrientation = React.useCallback((imageElement: HTMLImageElement): "left" | "right" => {
-    const nodeRect = imageElement.getBoundingClientRect()
-
-    return nodeRect.left + nodeRect.width / 2 < window.innerWidth / 2 ? "left" : "right"
-  }, [])
-
   const renderTeammateList = () =>
     teammates.map((teammate, i) => {
-
       return (
         <li key={i} teammate-ind={i} className={cn("relative")}>
+          <img className="rounded-full peer" src={teammate.img_path} alt="teammate image" />
           <img
-            className="rounded-full peer"
-            src={teammate.img_path}
-            alt="teammate image"
-          />
-          <img
-            className={cn("absolute bottom-0 right-0 opacity-0 invisible peer-hover:opacity-100 peer-hover:visible transition duration-300 ease-in-out")}
+            className={cn(
+              "absolute bottom-0 right-0 opacity-0 invisible peer-hover:opacity-100 peer-hover:visible transition duration-300 ease-in-out",
+            )}
             src="/assets/meet-the-team/teammate-pointer.svg"
             alt="teammate-pointer.svg"
           />
@@ -47,11 +25,7 @@ function TeamCarousel({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
               "pointer-events-none opacity-0 peer-hover:opacity-100 absolute top-full mt-20 left-4/2 -translate-x-1/2 z-40 w-[300px] transition-opacity duration-300 ease-in-out",
             )}
           >
-            {hoverState?.orientation === "left" ? (
-              <LeftTeamCard team={teammate.team} name={teammate.name} role={teammate.role} />
-            ) : (
-              <RightTeamCard team={teammate.team} name={teammate.name} role={teammate.role} />
-            )}
+            <TeamCard team={teammate.team} name={teammate.name} role={teammate.role} />
           </div>
         </li>
       )
