3340041de9e569ee76df63a669a8d2ee9e90ac9f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-16 07:19:53 +0100

    convert `ecosystem.config.cjs` to an ES module

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+import { env } from "node:process"
+
+const instanceName = env.INSTANCE_NAME
+if (!instanceName) throw new Error("INSTANCE_NAME is undefined")
+
+export const apps = [
+  {
+    name: `${instanceName}-server`,
+    script: "./server/dist/main.js",
+    interpreter: "/home/durhack/.nvm/versions/node/v24.9.0/bin/node",
+    env: {
+      NODE_ENV: "production",
+    },
+  },
+  {
+    name: `${instanceName}-ticket-assigner`,
+    script: "./server/dist/ticket-assigner.js",
+    interpreter: "/home/durhack/.nvm/versions/node/v24.9.0/bin/node",
+    env: {
+      NODE_ENV: "production",
+    },
+    instances: 1,
+    exec_mode: "fork",
+    cron_restart: "0-59/15 * * * *",
+    watch: false,
+    autorestart: false,
+  },
+]
