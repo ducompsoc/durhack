fb9ad07efeba5235f204994464f22834e15cd1bf
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-16 18:08:08 +0100

    sort out the travel details form

--- client/src/app/dashboard/application/travel/layout.tsx
+++ client/src/app/dashboard/application/travel/layout.tsx
@@ -3,7 +3,7 @@ import type * as React from "react"
 export default function TravelPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <h2 className="text-2xl">Travel Details</h2>
+      <h2 className="text-2xl">Travel</h2>
       {children}
     </>
   )
--- client/src/app/dashboard/application/travel/page.tsx
+++ client/src/app/dashboard/application/travel/page.tsx
@@ -4,9 +4,17 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation"
 import * as React from "react"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { z } from "zod/v4"
 
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@durhack/web-components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@durhack/web-components/ui/form"
 import { Input } from "@durhack/web-components/ui/input"
 import {
   Select,
@@ -16,6 +24,8 @@ import {
   SelectValue,
   SelectValueViewport,
 } from "@durhack/web-components/ui/select"
+
+import {travelOriginOptions, travelOriginSchema} from "@durhack/durhack-common/input/travel-origin"
 
 import { FormSkeleton } from "@/components/dashboard/form-skeleton"
 import { FormSubmitButton } from "@/components/dashboard/form-submit-button"
@@ -27,8 +37,8 @@ import { updateApplication } from "@/lib/update-application"
 type TravelDetailsFormFields = {
   travelOrigin: string
 }
-const TravelDetailsFormSchema = z.object({
-  travelOrigin: z.enum(["prefer-not-to-answer", "Durham", "elsewhere in the UK", "abroad"]),
+const travelDetailsFormSchema = z.object({
+  travelOrigin: travelOriginSchema,
 })
 /**
  * This component accepts <code>application</code> via props, rather than via
@@ -38,58 +48,47 @@ function TravelDetailsForm({ application }: { application: Application }) {
   const router = useRouter()
   const { mutateApplication } = useApplicationContext()
 
-  const form = useForm<TravelDetailsFormFields, unknown, z.infer<typeof TravelDetailsFormSchema>>({
-    resolver: zodResolver(TravelDetailsFormSchema),
+  const form = useForm<TravelDetailsFormFields, unknown, z.infer<typeof travelDetailsFormSchema>>({
+    resolver: zodResolver<TravelDetailsFormFields, unknown, z.infer<typeof travelDetailsFormSchema>>(travelDetailsFormSchema),
   })
-  async function onSubmit(values: z.infer<typeof TravelDetailsFormSchema>): Promise<void> {
+  async function onSubmit(values: z.infer<typeof travelDetailsFormSchema>): Promise<void> {
     await updateApplication("travel", values)
     await mutateApplication({ ...application, ...values })
-    //if (application.travelOrigin == null) router.push("/dashboard/cv")
+    if (application.travelOrigin == null) router.push("/dashboard/application/submit")
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="lg:columns-2">
-          <div className="mb-4">
-            <FormField
-              control={form.control}
-              name="travelOrigin"
-              render={({ field: { onChange, value, ref, ...field } }) => (
-                <FormItem>
-                  <FormLabel>Where will you be travelling from</FormLabel>
-                  <div className="flex">
-                    <Select onValueChange={onChange} value={value} {...field}>
-                      <FormControl>
-                        <SelectTrigger ref={ref}>
-                          <SelectValueViewport>
-                            <SelectValue />
-                          </SelectValueViewport>
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="prefer-not-to-answer">Prefer Not To Say</SelectItem>
-                        <SelectItem value="less-than-5-miles">Durham</SelectItem>
-                        <SelectItem value="5-to-20-miles">&lt;20 miles (e.g. Newcastle)</SelectItem>
-                        <SelectItem value="20-to-60-miles">20-60 miles (e.g. Middlesbrough)</SelectItem>
-                        <SelectItem value="60-to-150-miles">60-150 miles (e.g. Lancaster)</SelectItem>
-                        <SelectItem value="more-than-150-miles">&ge;150 miles (e.g. Oxford, London)</SelectItem>
-                        <SelectItem value="abroad">Abroad</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    {value === "elsewhere-in-the-uk" && (
-                      <Input className="ml-4" placeholder="enter a UK city/town..." />
-                    )}
-                    {value === "prefer-not-to-answer" && (
-                      <p className="ml-4 text-sm text-red-600">
-                        You will not be able to apply for travel reimbursement.
-                      </p>
-                    )}
-                  </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+        <div className="mb-4">
+          <FormField
+            control={form.control}
+            name="travelOrigin"
+            render={({ field: { onChange, value, ref, ...field } }) => (
+              <FormItem>
+                <FormLabel>Travel Origin</FormLabel>
+                <FormDescription>Where will you be travelling from?</FormDescription>
+                <Select onValueChange={onChange} value={value} {...field}>
+                  <FormControl>
+                    <SelectTrigger ref={ref}>
+                      <SelectValueViewport>
+                        <SelectValue />
+                      </SelectValueViewport>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {travelOriginOptions.map((option) => <SelectItem value={option.value}>{option.label}</SelectItem>)}
+                  </SelectContent>
+                </Select>
+                {value === "prefer-not-to-answer" && (
+                  <p className="ml-4 text-sm text-orange-300">
+                    Warning: selecting this option disqualifies you from our travel reimbursement scheme.
+                  </p>
+                )}
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="mt-16 flex justify-center">
           <FormSubmitButton type="submit">Save Progress</FormSubmitButton>
