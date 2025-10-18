9e35aa32f61131bb3fd47eddc6ae85a1f159d6d5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-18 01:36:14 +0100

    port must be specified when `nginx` isn't being used

--- client/src/config/site.ts
+++ client/src/config/site.ts
@@ -36,6 +36,7 @@ if (isDevelopment) {
 
 if (isDevelopment && noLocalServer) {
   Object.assign(siteConfig, {
+    url: "http://durhack-staging.com:3000",
     apiUrl: "https://api.durhack-staging.com",
   })
 }
