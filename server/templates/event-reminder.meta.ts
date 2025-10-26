d9c6bc418c921887631901f22bcfcf08ef441c0a
“technologeve” <eve.nicole@icloud.com>
2025-10-26 20:58:46 +0000

    Undo these changes?

--- server/templates/event-reminder.meta.ts
+++ server/templates/event-reminder.meta.ts
@@ -1,5 +1,5 @@
 import type { TemplateMetadataIn } from "@/mailer/template-metadata"
 
 export default {
-  messageTitle: "💻 Ready for DurHack? 🌞",
+  messageTitle: "🕺 Ready for DurHack? 💻",
 } satisfies TemplateMetadataIn
