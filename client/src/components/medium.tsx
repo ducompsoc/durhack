81492c2208bf8620d286e5cee1bfad2cd8b68acc
ItzTJSV2 <ItzTJSV2@gmail.com>
2025-10-28 00:15:24 +0000

    Make text apart of image

--- client/src/components/medium.tsx
+++ client/src/components/medium.tsx
@@ -7,7 +7,7 @@ export function Medium({ className, ...props }: React.HTMLAttributes<HTMLDivElem
     return (
         <div
       className={cn(
-        "medium h-[calc(10rem+18vw)] sm:h-100 md:h-120 lg:h-140 xl:h-180 2xl:h-250 relative flex items-start justify-center overflow-hidden",
+        "medium h-[calc(16rem+18vw)] sm:h-100 md:h-120 lg:h-140 xl:h-180 2xl:h-280 relative flex items-start justify-center overflow-hidden",
         className,
       )}
       {...props}
@@ -19,38 +19,46 @@ export function Medium({ className, ...props }: React.HTMLAttributes<HTMLDivElem
           alt="Left Polaroid"
           width={376}
           height={479}
-          className="absolute left-[15%] top-40 w-1/5 max-w-150"
+          className="absolute left-[15%] top-[30%] w-1/5 max-w-150"
         />
         <Image
           src="/assets/medium/stickyNote.svg"
           alt="Sticky Note"
           width={712}
           height={774}
-          className="absolute left-[40%] top-50 w-1/3 max-w-200"
+          className="absolute left-[40%] top-[15%] w-1/3 max-w-200"
         />
         <Image
           src="/assets/medium/middlePolaroid.svg"
           alt="Middle Polaroid"
           width={397}
           height={494}
-          className="absolute left-[30%] top-50 w-1/5 max-w-150"
+          className="absolute left-[30%] top-[10%] w-1/5 max-w-150"
         />
         <Image
           src="/assets/medium/rightPolaroid.svg"
           alt="Right Polaroid"
           width={374}
           height={478}
-          className="absolute right-[12%] top-100 w-1/5 max-w-150"
+          className="absolute right-[12%] top-[30%] w-1/5 max-w-150"
         />
-
-        <div className="absolute left-[49%] top-85 w-1/5 max-w-200">
-            <h2 className={cn(
-                patrickHand.className,
-                "section-header text-[#006793] text-center uppercase w-fit rotate-13 text-[clamp(1.0rem,1.0vw+0.1rem,1.8rem)]",
-                className,
-            )}>DurHack, Durham University’s flagship student-run hackathon, has ignited in a nation of computer science enthusiasts. The hackathon, which brings students together to build tech projects over 24-hours, has quickly become one of the most inspiring events of its kind to encourage innovation, collaboration, and of course: coding for good.
-            </h2>
-        </div>
+        <a href="https://medium.com/@DurHack_press/coding-for-good-in-durham-how-hackathons-are-building-the-next-generation-of-problem-solvers-bd5baf57fcef" target="_blank" rel="noopener noreferrer">
+            <div className="absolute right-[5%] text-center bottom-[10%] w-1/3 max-w-200">
+                <p className={cn(
+                    patrickHand.className,
+                    "section-header font-bold text-[#006793] tracking-widest text-center uppercase w-fit text-[clamp(1.1rem,1.1vw+0.3rem,1.9rem)]",
+                    className,
+                    )}>READ MORE &rarr;
+                </p>
+                <Image
+                    src="/assets/medium/mediumLink.svg"
+                    alt="Medium Hyperlink"
+                    width={286}
+                    height={44}
+                    className="mx-auto w-1/3 max-w-150"
+                />
+            </div>
+        </a>
       </div>
       
       <div className="absolute h-full 2xl:hidden w-full top-0">
@@ -59,38 +67,46 @@ export function Medium({ className, ...props }: React.HTMLAttributes<HTMLDivElem
           alt="Left Polaroid"
           width={376}
           height={479}
-          className="absolute left-[15%] top-50 w-1/5 max-w-150"
+          className="absolute left-[15%] top-40 w-1/5 max-w-150"
         />
         <Image
           src="/assets/medium/stickyNote.svg"
           alt="Sticky Note"
           width={712}
           height={774}
-          className="absolute left-[40%] top-15 w-1/3 max-w-200"
+          className="absolute left-[39%] top-5 w-1/3 max-w-200"
         />
         <Image
           src="/assets/medium/middlePolaroid.svg"
           alt="Middle Polaroid"
           width={397}
           height={494}
-          className="absolute left-[30%] top-15 w-1/5 max-w-150"
+          className="absolute left-[28%] top-5 w-1/5 max-w-150"
         />
         <Image
           src="/assets/medium/rightPolaroid.svg"
           alt="Right Polaroid"
           width={374}
           height={478}
-          className="absolute right-[12%] top-50 w-1/5 max-w-150"
+          className="absolute right-[12%] top-25 w-1/5 max-w-150"
         />
-
-        <div className="absolute left-[49%] top-45 w-1/5 max-w-200">
-            <h2 className={cn(
-                patrickHand.className,
-                "section-header text-[#006793] text-center uppercase w-fit rotate-13 text-[clamp(0.2rem,0.9vw+0.1rem,1.0rem)]",
-                className,
-            )}>DurHack, Durham University’s flagship student-run hackathon, has ignited in a nation of computer science enthusiasts. The hackathon, which brings students together to build tech projects over 24-hours, has quickly become one of the most inspiring events of its kind to encourage innovation, collaboration, and of course: coding for good.
-            </h2>
-        </div>
+        <a href="https://medium.com/@DurHack_press/coding-for-good-in-durham-how-hackathons-are-building-the-next-generation-of-problem-solvers-bd5baf57fcef" target="_blank" rel="noopener noreferrer">
+            <div className="absolute right-[5%] text-center bottom-[10%] w-1/3 max-w-200">
+                <p className={cn(
+                    patrickHand.className,
+                    "section-header font-bold text-[#006793] tracking-widest text-center uppercase w-fit text-[clamp(1.1rem,1.1vw+0.3rem,1.9rem)]",
+                    className,
+                    )}>READ MORE &rarr;
+                </p>
+                <Image
+                    src="/assets/medium/mediumLink.svg"
+                    alt="Medium Hyperlink"
+                    width={286}
+                    height={44}
+                    className="mx-auto w-1/3 max-w-150"
+                />
+            </div>
+        </a>
       </div>
 
     </div>
