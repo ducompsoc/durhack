e4f09f071524e23f248291cbed26665bf1f98694
ItzTJSV2 <ItzTJSV2@gmail.com>
2025-10-28 00:44:33 +0000

    Adaptable text size

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -7,7 +7,7 @@ import { siteConfig } from "@/config/site";
 
 function SocialsBar() {
   return (
-    <div className="flex gap-1 sm:gap-2 md:gap-4 items-center justify-center mt-4">
+    <div className="flex gap-1 sm:gap-1 md:gap-1 items-center justify-center mt-4">
       {siteConfig.socials.map((social) => {
         const Icon = social.icon;
         return (
@@ -16,7 +16,7 @@ function SocialsBar() {
             href={social.href}
             target="_blank"
             rel="noopener noreferrer"
-            className="p-1 sm:p-2 md:p-3 rounded-full hover:bg-gray-200 transition-colors"
+            className="p-1 sm:p-2 md:p-3 rounded-full"
           >
             <Icon className="w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8 text-[#006793]" />
           </Link>
@@ -45,20 +45,20 @@ export function Footer ({ className, ...props }: React.HTMLAttributes<HTMLDivEle
   return (
     <div
       className={cn(
-        "footer h-[calc(40rem+20vw)] sm:h-180 md:h-180 lg:h-180 xl:h-170 2xl:h-180 relative flex items-start justify-center overflow-hidden",
+        "footer h-[calc(40rem+20vw)] sm:h-180 md:h-180 lg:h-180 xl:h-190 2xl:h-210 relative flex items-start justify-center overflow-hidden",
         className,
       )}
       {...props}
     >
       <div className="absolute h-full hidden 2xl:contents 2xl:w-full top-10">
-        <CloudsFooterGraphic className="absolute left-[-5%] top-105 w-full" />
+        <CloudsFooterGraphic className="absolute left-[-5%] top-[10%] w-full" />
       </div>
 
       <div className="absolute left-[-5%] left-[0%] top-[180%] w-[140%]">
         <CloudsFooterGraphic className="absolute left-[-5%] bottom-0 w-full" />
       </div>
 
-      <div className="relative z-10 rounded-3xl w-[90%] h-[100%] translate-y-32 flex flex-col items-center px-8 py-12 sm:py-16" style={{ backgroundColor: "#DCF4FF" }}>
+      <div className="relative z-10 rounded-3xl w-[90%] h-[90%] translate-y-32 flex flex-col items-center px-8 py-12 sm:py-16" style={{ backgroundColor: "#DCF4FF" }}>
         
         <div>
           <Image
@@ -66,10 +66,10 @@ export function Footer ({ className, ...props }: React.HTMLAttributes<HTMLDivEle
             alt="Durhack Logo"
             width={416}
             height={115}
-            className="absolute left-[10%] top-35 w-1/4 max-w-150"
+            className="absolute left-[10%] top-[20%] w-1/4 max-w-150"
           />
 
-          <div className="absolute items-center text-center right-[10%] top-20 max-w-250">
+          <div className="absolute items-center text-center right-[10%] top-[10%] max-w-250">
             <h2 className={cn(
               patrickHand.className,
               "section-header text-[#006793] uppercase text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold w-fit",
@@ -86,14 +86,14 @@ export function Footer ({ className, ...props }: React.HTMLAttributes<HTMLDivEle
         <div className="relative items-center text-center bottom-[-10%]">
             <p className={cn(
               spaceGrotesk.className,
-              "text-[#006793] text-sm font-normal px-",
+              "text-[#006793] text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal px-6",
             )}>
               DurHack follows the {mlhCodeOfConduct}. Photos and videos taken at events in 2024.
             </p>
             <br></br>
             <p className={cn(
               spaceGrotesk.className,
-              "text-[#006793] text-sm font-normal px-6",
+              "text-[#006793] text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal px-6",
             )}>
               DurHack is an event hosted by Durham University Computing Society ({CompSoc}), which is a student society affiliated with Durham Students' Union ({StudentUnion}). Durham Students' Union is registered in England as a company limited by guarantee (07689815) and a charity (1145400), with VAT number 119733690 and registered office Dunelm House, New Elvet, Durham DH1 3AN.
             </p>
