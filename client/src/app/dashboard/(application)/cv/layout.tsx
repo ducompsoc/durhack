1b8f7e6e03e859f9706f5e5d9688a0181edaa089
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-28 06:56:02 +0000

    provide cv context to cv page

--- client/src/app/dashboard/(application)/cv/layout.tsx
+++ client/src/app/dashboard/(application)/cv/layout.tsx
@@ -1,10 +1,12 @@
 import type * as React from "react"
 
+import { ApplicationCvContextProvider } from "@/components/dashboard/application-cv-context-provider"
+
 export default function CvPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ApplicationCvContextProvider>
       <h2 className="text-2xl">CV Submission</h2>
       {children}
-    </>
+    </ApplicationCvContextProvider>
   )
 }
