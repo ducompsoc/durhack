a1f2a15282109d1ee55a76579bed40464ee1f6ad
Emre Ozden <tlcj35@durham.ac.uk>
2026-08-16 20:58:26 +0100

    removed boilerplate

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -13,36 +13,32 @@ export default function HomePage() {
           <h1 className="text-white text-5xl font-bold">Hero Section</h1>
         </div>
         {/* Getting There */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Getting there</h1>
         </div>
         {/* Info */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Info</h1>
         </div>
         {/* Sponsors */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Sponsors</h1>
         </div>
         {/* Partners */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Partners</h1>
         </div>
         {/* FAQ */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">FAQ</h1>
         </div>
         {/* Past Projects */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Past Projects</h1>
         </div>
         {/* Meet The Team */}
-        <div className="flex items-start justify-center">
+        <div className="flex items-start justify-center hidden">
           <h1 className="text-white text-5xl font-bold">Meet the Team</h1>
-        </div>
-        {/* Footer */}
-        <div className="flex items-start justify-center">
-          <h1 className="text-white text-5xl font-bold">Footer</h1>
         </div>
       </main>
     </>
