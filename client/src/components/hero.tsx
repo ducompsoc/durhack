3542f15f60d13446b120a9859e6bff1d727f301b
Emre Ozden <tlcj35@durham.ac.uk>
2026-08-23 19:57:54 +0100

    fixed sun rays color issue and made them fit viewport dimensions without aspect ratio, moved sun lower for small screens

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 export default function Hero() {
   return (
-    <div className="grid grid-rows-2 items-stretch justify-center h-screen bg-[#EB3DA2] max-h-screen relative overflow-hidden">
+    <div className="grid grid-rows-2 items-stretch justify-center h-screen bg-[#E566B0] max-h-screen relative overflow-hidden">
       {/* Top half */}
       <div className="z-10 flex flex-col justify-center items-center gap-8 h-full w-full">
         <div className="flex flex-col items-center justify-center gap-10 md:gap-15">
@@ -50,19 +50,8 @@ export default function Hero() {
       </div>
 
       {/* Bottom half */}
-      <div className="flex-1 z-0 flex justify-center items-center w-full">
+      <div className="flex-1 z-10 flex justify-center items-center w-full">
         <div className="relative w-full flex justify-center items-center ">
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[85%] w-[100vmax] min-w-[1000px] h-auto aspect-square">
-            <Image
-              className="object-contain"
-              width={100}
-              height={100}
-              layout="responsive"
-              priority
-              alt="sun rays"
-              src="/assets/hero/sun-rays.svg"
-            />
-          </div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[12%] w-[100vmax] min-w-[1000px] z-1 h-auto aspect-square">
             <Image
               className="object-contain"
@@ -74,12 +63,16 @@ export default function Hero() {
               src="/assets/hero/buildings.svg"
             />
           </div>
-          <div className="relative w-[clamp(300px,80vw,900px)] aspect-square flex justify-center items-center translate-y-[10vmax]">
+          <div className="relative w-[clamp(300px,80vw,900px)] aspect-square flex justify-center items-center translate-y-[25vmax] md:translate-y-[10vmax]">
             <Image className="object-contain" fill priority alt="sun" src="/assets/hero/sun.svg" />
             <Image className="object-contain" fill priority alt="sun aura" src="/assets/hero/sun-aura.svg" />
           </div>
         </div>
       </div>
-    </div >
+
+      <div className="absolute inset-0 w-full h-full pointer-events-none z-0">
+        <Image className="object-fill" fill priority alt="sun rays" src="/assets/hero/sun-rays.svg" />
+      </div>
+    </div>
   )
 }
