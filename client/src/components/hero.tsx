5d3b5b181644aafced852f4037f39c0d07bd0d80
Emre Ozden <tlcj35@durham.ac.uk>
2026-08-26 22:04:35 +0100

    minor positioning adjustments

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -67,7 +67,7 @@ export default function Hero() {
               src="/assets/hero/buildings.svg"
             />
           </div>
-          <div className="relative w-[clamp(300px,80vw,900px)] aspect-square flex justify-center items-center z-0 translate-y-[10vmax] md:translate-y-[0vmax]">
+          <div className="relative w-[clamp(300px,80vw,900px)] aspect-square flex justify-center items-center z-0 translate-y-[10vh]">
             <Image className="object-contain" fill priority alt="sun" src="/assets/hero/sun.svg" />
             <Image className="object-contain" fill priority alt="sun aura" src="/assets/hero/sun-aura.svg" />
           </div>
