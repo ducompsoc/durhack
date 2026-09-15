4ebc644d6642b47a53f7fe60ba561c06aae51647
Emre Ozden <tlcj35@durham.ac.uk>
2026-09-15 19:55:52 +0100

    refactored guilds section to fit current requirements and added relevant assets

--- client/src/components/section-header.tsx
+++ client/src/components/section-header.tsx
@@ -10,7 +10,7 @@ export function SectionHeader({ className, ...props }: React.HTMLAttributes<HTML
     <h1
       className={cn(
         audiowide.className,
-        "section-header text-[#006793] uppercase text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold w-fit",
+        "section-header text-white uppercase text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold w-fit",
         className,
       )}
       {...props}
