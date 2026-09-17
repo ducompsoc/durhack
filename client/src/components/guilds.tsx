3db851711d98168043d4cced26136d3ffe6574c0
Emre Ozden <tlcj35@durham.ac.uk>
2026-09-17 19:55:27 +0100

    guilds: added text and re-factored

--- client/src/components/guilds.tsx
+++ client/src/components/guilds.tsx
@@ -6,18 +6,27 @@ import { guilds } from "@/config/guilds"
 import { cn } from "@/lib/utils"
 
 import "@/styles/guilds.css"
+import {spaceGrotesk} from "@/lib/google-fonts";
+
+function GuildsText({innerText, className, ...props}: { innerText: string } & React.ComponentProps<"p">) {
+ return (
+   <p className={cn(spaceGrotesk.className, "py-5 text-center text-2xl", className)}>{innerText}</p>
+ )
+}
 
 export function Guilds({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div className={cn("guilds flex items-start justify-center", className)} {...props}>
-      <div className="flex-row justify-center items-center w-90 sm:w-135 md:w-180">
+    <div className={cn("flex flex-col content-center items-start justify-center", className)} {...props}>
+      <div className="flex-row justify-center items-center self-center container">
         <SectionHeader>Guilds</SectionHeader>
 
-        <div className="my-10 align-center justify-center flex flex-col">
-          {guilds.map((guild, _index) => (
-            <Image src={guild.imgSrc} key={guild.name} alt={`${guild.name} tile`} />
-          ))}
-        </div>
+        <GuildsText innerText={"Once again, we are bringing back guilds! Think of them like hackathon houses where you’ll take on challenges as a team and earn rewards in addition to your group project."}/>
+        <GuildsText innerText={"This year, we have transitioned from four to three guilds, more closely following the layout of our venue. The three floors will be home to the following guilds:"}/>
+      </div>
+      <div className="py-5 align-center justify-center self-center w-90 sm:w-135 md:w-180 flex flex-col">
+        {guilds.map((guild, _index) => (
+          <Image src={guild.imgSrc} key={guild.name} alt={`${guild.name} tile`} />
+        ))}
       </div>
     </div>
   )
