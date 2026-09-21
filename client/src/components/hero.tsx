ac3497908b9c122212aac7615a1dd000149fee10
Emre Ozden <tlcj35@durham.ac.uk>
2026-09-21 20:13:51 +0100

    added title padding for hero text

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -38,7 +38,7 @@ export default function Hero() {
       <div className="grid grid-rows-2 items-stretch justify-center bg-[#E566B0] min-h-screen relative">
         {/* Top half */}
         <div className="z-10 flex flex-col justify-center items-center w-full">
-          <div className="w-full max-w-4xl flex justify-center items-center pb-6">
+          <div className="w-full max-w-4xl flex justify-center items-center py-6">
             <DurhackTitle />
           </div>
           <div className="w-full max-w-1/2 mb-8 flex justify-center items-center">
