974b3a7a735f4f31973f4efaf730d1cbbdf1e468
Emre Ozden <tlcj35@durham.ac.uk>
2026-09-26 01:08:14 +0100

    converted all images to webp and lowered resolution

--- client/src/config/teammates.tsx
+++ client/src/config/teammates.tsx
@@ -9,7 +9,7 @@ export type Teammate = {
 
 export const teammates: Teammate[] = [
   {
-    imgSrc: "/assets/meet-the-team/rebecca.jpg",
+    imgSrc: "/assets/meet-the-team/rebecca.webp",
     teammateName: "Rebecca",
     teamPosition: "Lead Organiser",
     funFact: "My hair has been every colour of the rainbow atleast twice (almost)",
@@ -17,14 +17,14 @@ export const teammates: Teammate[] = [
     quoteSource: "Neil deGrasse Tyson"
   },
   {
-    imgSrc: "/assets/meet-the-team/oscar.png",
+    imgSrc: "/assets/meet-the-team/oscar.webp",
     teammateName: "Oscar",
     teamPosition: "Co-Lead",
     funFact: "I've been to over 35 Hackathons!",
     quote: "80% of statistics are made up",
   },
   {
-    imgSrc: "/assets/meet-the-team/palak.png",
+    imgSrc: "/assets/meet-the-team/palak.webp",
     teammateName: "Palak",
     teamPosition: "Sponsorship Head",
     funFact: "I am learning to speak German",
@@ -32,7 +32,7 @@ export const teammates: Teammate[] = [
     quoteSource: "No reference needed"
   },
   {
-    imgSrc: "/assets/meet-the-team/ella.png",
+    imgSrc: "/assets/meet-the-team/ella.webp",
     teammateName: "Ella",
     teamPosition: "Logistics",
     funFact: "I go Scottish dancing once a week and it’s the best thing ever",
@@ -40,7 +40,7 @@ export const teammates: Teammate[] = [
     quoteSource: "Roy, IT Crowd"
   },
     {
-      imgSrc: "/assets/meet-the-team/oykunaz.jpeg",
+      imgSrc: "/assets/meet-the-team/oykunaz.webp",
       teammateName: "Öykünaz",
       teamPosition: "Sponsorship",
       funFact: "I write the etymology of words I find interesting and their stories.",
@@ -48,7 +48,7 @@ export const teammates: Teammate[] = [
       quoteSource: "John Steinbeck"
     },
     {
-      imgSrc: "/assets/meet-the-team/joe.jpeg",
+      imgSrc: "/assets/meet-the-team/joe.webp",
       teammateName: "Joe",
       teamPosition: "Logistics",
       funFact: "I think pizza is criminally overrated.",
@@ -56,14 +56,14 @@ export const teammates: Teammate[] = [
       quoteSource: "Jeremy Clarkson",
     },
     {
-      imgSrc: "/assets/meet-the-team/hannah.JPEG",
+      imgSrc: "/assets/meet-the-team/hannah.webp",
       teammateName: "Hannah",
       teamPosition: "Hacker Experience ",
       quote: "print(\"Hello World!\") - it got rejected in my yearbook",
       funFact: "I was interviewed on the main screen at the London Paralympics swimming event!"
     },
     {
-      imgSrc: "/assets/meet-the-team/grishma.jpg",
+      imgSrc: "/assets/meet-the-team/grishma.webp",
       teammateName: "Grishma Velivala",
       teamPosition: "Sponsorship Officer",
       funFact: "Love to chat for hours :)",
@@ -71,7 +71,7 @@ export const teammates: Teammate[] = [
       quoteSource: "Albert Einstein"
     },
     {
-      imgSrc: "/assets/meet-the-team/pia.jpeg",
+      imgSrc: "/assets/meet-the-team/pia.webp",
       teammateName: "Pia",
       teamPosition: "Design",
       funFact: "I love combining creativity with technology, and I’m currently developing an app idea designed to support students and improve their university experience.",
@@ -79,7 +79,7 @@ export const teammates: Teammate[] = [
       quoteSource: "Alan Kay",
     },
     {
-      imgSrc: "/assets/meet-the-team/millie.jpeg",
+      imgSrc: "/assets/meet-the-team/millie.webp",
       teammateName: "Millie",
       teamPosition: "Marketing ",
       funFact: "I’ve been bitten by a stingray ",
